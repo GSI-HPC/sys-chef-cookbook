17d77b13ea5e4bd713a96b12315414fb94671cd1
Matthias Pausch <mpausch@lxdv60.gsi.de>
2013-03-06 14:28:24 +0100

    secret should be more than a minor version increase

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "vic.penso@gmail.com"
 license          "Apache 2.0"
 description      "System Software configuration and maintenance"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.8.1"
+version          "0.9.0"
 
