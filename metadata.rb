5b5a416e77c2d05394da277d9cdcbd39ca3e0cf4
Matthias Pausch <user@localhost>
2013-03-28 15:31:41 +0100

    add recipes for kerberos/ldap authentication

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "vic.penso@gmail.com"
 license          "Apache 2.0"
 description      "System Software configuration and maintenance"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.9.0"
+version          "0.10.0"
 
