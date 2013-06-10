61f0db5fa3c8446a5dec1542c199ee51f801d43b
Dennis Klein <d.klein@gsi.de>
2013-06-10 21:45:09 +0200

    version bump to 0.11.1

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "vic.penso@gmail.com"
 license          "Apache 2.0"
 description      "System Software configuration and maintenance"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.11.0"
+version          "0.11.1"
