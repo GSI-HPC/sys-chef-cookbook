9539a466c2d2c770ec6315b2fc36b110c7c50c9b
Dennis Klein <d.klein@gsi.de>
2013-07-26 18:21:26 +0200

    set default attributes for apt keys

--- attributes/apt.rb
+++ attributes/apt.rb
@@ -2,3 +2,4 @@ default_unless[:sys][:apt][:config] = Hash.new
 default_unless[:sys][:apt][:preferences] = Hash.new
 default_unless[:sys][:apt][:repositories] = Hash.new
 default_unless[:sys][:apt][:sources] = String.new
+default_unless[:sys][:apt][:keys] = { :add => Hash.new, :remove => Hash.new }
