0df152e7631513b9c0700964c86424d71a7454fb
Dennis Klein <d.klein@gsi.de>
2013-07-31 16:05:58 +0200

    fix default attributes

--- attributes/apt.rb
+++ attributes/apt.rb
@@ -2,4 +2,5 @@ default_unless[:sys][:apt][:config] = Hash.new
 default_unless[:sys][:apt][:preferences] = Hash.new
 default_unless[:sys][:apt][:repositories] = Hash.new
 default_unless[:sys][:apt][:sources] = String.new
-default_unless[:sys][:apt][:keys] = { :add => Hash.new, :remove => Hash.new }
+default_unless[:sys][:apt][:keys][:add] = Array.new
+default_unless[:sys][:apt][:keys][:remove] = Array.new
