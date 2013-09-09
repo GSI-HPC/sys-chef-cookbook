db0f87befde84b942b7ebcea77cd99046aed2edb
Christopher Huhn <C.Huhn@gsi.de>
2013-09-09 18:41:55 +0200

    Check for non-existant groups during user creation

--- recipes/accounts.rb
+++ recipes/accounts.rb
@@ -22,18 +22,30 @@ unless node.sys.accounts.empty?
   package 'ruby-shadow'
 
   node.sys.accounts.each do |name,account|
-    # This is a wrapper to call the `user` resource by attributes
-    user name do
-      uid        account[:uid]       if account.has_key?(:uid)
-      gid        account[:gid]       if account.has_key?(:gid)
-      home       account[:home]      if account.has_key?(:home)
-      shell      account[:shell]     if account.has_key?(:shell)
-      password   account[:password]  if account.has_key?(:password)
-      comment    account[:comment]   if account.has_key?(:comment)
-      supports(  account[:supports]) if account.has_key?(:supports)
-      system     true                if account.has_key?(:system) and account[:system]
-      action     account[:action]    if account.has_key?(:action)
+
+    # check if the given group exists, gid could be numeric or string:
+    if account.has_key?(:gid) and not
+        (node.etc.group.has_key?(account[:gid]) or node[:etc][:group].values.detect { |e| e[:gid] == account[:gid] })
+
+      log("The given group '#{account[:gid]}' does not exist - creation of user '#{name}' skipped"){ level :error }
+
+    else
+
+      # This is a wrapper to call the `user` resource by attributes
+      #  I got the impression this is rather boiler-plate?!
+      user name do
+        uid        account[:uid]       if account.has_key?(:uid)
+        gid        account[:gid]       if account.has_key?(:gid)
+        home       account[:home]      if account.has_key?(:home)
+        shell      account[:shell]     if account.has_key?(:shell)
+        password   account[:password]  if account.has_key?(:password)
+        comment    account[:comment]   if account.has_key?(:comment)
+        supports(  account[:supports]) if account.has_key?(:supports)
+        system     true                if account.has_key?(:system) and account[:system]
+        action     account[:action]    if account.has_key?(:action)
+      end
+
     end
   end
 
-end 
+end
