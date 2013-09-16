f20be6cc2f25104b120cadafd6f3ff60770dfe68
Christopher Huhn <C.Huhn@gsi.de>
2013-09-16 11:40:45 +0200

    Add multiarch configuration

--- recipes/apt.rb
+++ recipes/apt.rb
@@ -84,3 +84,11 @@ unless node.sys.apt[:keys].add.empty?
     sys_apt_key key
   end
 end
+
+# add multiarch support if desired:
+#  this is statically pinned to i386 on amd64 for now
+execute 'dpkg --add-architecture i386' do   
+  only_if { node[:sys][:apt][:multiarch] and node[:debian][:architecture] == 'amd64' }
+  not_if  { node[:debian][:foreign_architectures].include?('i386') }
+  notifies :run, "execute[#{apt_update}]", :immediately
+end
