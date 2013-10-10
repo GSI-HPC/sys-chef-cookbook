4dcb9156af345b3271a7e8c97883fd65c0576f0f
Dennis Klein <d.klein@gsi.de>
2013-10-11 00:19:12 +0200

    FC045: Consider setting cookbook name in metadata: ./metadata.rb:1, bump version to 0.18.2

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
+name             "sys"
 maintainer       "GSI HPC department"
 maintainer_email "hpc@gsi.de"
 license          "Apache 2.0"
 description      "System Software configuration and maintenance"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.18.1"
+version          "0.18.2"
