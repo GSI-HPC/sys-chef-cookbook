a62980af16a8f37d4f520090bde536045921839c
Christopher Huhn <C.Huhn@gsi.de>
2014-01-29 13:48:40 +0100

    Start a recipe for clamav virus scanner

---
+++ recipes/clamav.rb
@@ -0,0 +1,17 @@
+#
+# recipe for clamav virus scanner
+#
+
+package 'clamav'
+
+params = { }
+
+if node[:http_proxy]
+  params[:proxy_srv]  = node[:http_proxy][:name]
+  params[:proxy_name] = node[:http_proxy][:port]
+end
+
+template '/etc/clamav/freshclam.conf' do
+  source 'etc_clamav_freshclam.conf.erb'
+  variables params
+end
