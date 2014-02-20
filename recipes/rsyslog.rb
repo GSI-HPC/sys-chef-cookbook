17edf58294142284749df06b979a67de857bdad9
Christopher Huhn <C.Huhn@gsi.de>
2014-02-20 17:24:31 +0100

    Add recipe for loghost config

---
+++ recipes/rsyslog.rb
@@ -0,0 +1,26 @@
+#
+# extremly basic loghost setup
+#
+
+if node[:rsyslog].has_key?('server_ip')
+  
+  loghost_line = "*.*\t@"
+  loghost_line =+ '@' if node['rsyslog']['protocol'] == 'tcp'
+  loghost_line =+ node[:rsyslog][:server_ip]
+  if node['rsyslog'].has_key?('port')
+    loghost_line =+ node['rsyslog']['port'] unless node['rsyslog']['port'] == 514
+  end
+  
+  # forward everything to the loghost:
+  file '/etc/rsyslog.d/loghost.conf' do
+    content = "#{loghost_line}\n"
+    notifies  :restart, "service[rsyslog]"
+  end
+
+  service "rsyslog" do
+    supports :restart => true, :reload => true, :status => true
+    action   [:enable, :start]
+  end
+
+end
+
