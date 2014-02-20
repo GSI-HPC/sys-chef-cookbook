fbb515ebd267efbb5a9d4a957a23bad6e4c57805
Christopher Huhn <C.Huhn@gsi.de>
2014-02-20 18:12:18 +0100

    Fixes and VB

--- recipes/rsyslog.rb
+++ recipes/rsyslog.rb
@@ -5,22 +5,21 @@
 if node[:rsyslog].has_key?('server_ip')
   
   loghost_line = "*.*\t@"
-  loghost_line =+ '@' if node['rsyslog']['protocol'] == 'tcp'
-  loghost_line =+ node[:rsyslog][:server_ip]
+  loghost_line += '@' if node['rsyslog']['protocol'] == 'tcp'
+  loghost_line += node[:rsyslog][:server_ip]
   if node['rsyslog'].has_key?('port')
-    loghost_line =+ node['rsyslog']['port'] unless node['rsyslog']['port'] == 514
+    loghost_line += node['rsyslog']['port'] unless node['rsyslog']['port'] == 514
   end
   
   # forward everything to the loghost:
   file '/etc/rsyslog.d/loghost.conf' do
-    content = "#{loghost_line}\n"
-    notifies  :restart, "service[rsyslog]"
+    content  "#{loghost_line}\n"
+    notifies :restart, "service[rsyslog]"
   end
 
   service "rsyslog" do
-    supports :restart => true, :reload => true, :status => true
+    supports :restart => true, :status => true
     action   [:enable, :start]
   end
 
 end
-
