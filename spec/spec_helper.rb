9f5903b4303331361c2cf2a54b87ccc37f24d735
Dennis Klein <d.klein@gsi.de>
2014-03-12 01:29:19 +0100

    add some test coverage for sys::sudo

---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+require 'chefspec'
+ChefSpec::Coverage.start!
+
+RSpec.configure do |config|
+  # Specify the path for Chef Solo to find cookbooks (default: [inferred from
+  # the location of the calling spec file])
+  #config.cookbook_path = '/var/cookbooks'
+
+  # Specify the path for Chef Solo to find roles (default: [ascending search])
+  #config.role_path = '/var/roles'
+
+  # Specify the Chef log_level (default: :warn)
+  #config.log_level = :debug
+
+  # Specify the path to a local JSON file with Ohai data (default: nil)
+  #config.path = 'ohai.json'
+
+  # Specify the operating platform to mock Ohai data from (default: nil)
+  config.platform = 'debian'
+
+  # Specify the operating version to mock Ohai data from (default: nil)
+  config.version = '7.4'
+end
