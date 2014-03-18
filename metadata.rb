e3052fe9d15424b33ae5475c428016615bff5f79
Matthias Pausch <m.pausch@gsi.de>
2014-03-18 16:45:24 +0100

    specify attribute precedence

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "hpc@gsi.de"
 license          "Apache 2.0"
 description      "System Software configuration and maintenance"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.28.0"
+version          "0.28.1"
