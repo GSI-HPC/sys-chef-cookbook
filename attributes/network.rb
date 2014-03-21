32a362b4f95fb4e7f220ba0c043147f00445cab1
Dennis Klein <d.klein@gsi.de>
2014-03-21 17:54:46 +0100

    add definition for creating vlan bridges

--- attributes/network.rb
+++ attributes/network.rb
@@ -1,3 +1,4 @@
 default_unless[:sys][:network][:interfaces] = Mash.new
 default_unless[:sys][:network][:restart] = true
 default_unless[:sys][:network][:keep_interfaces] = false
+default_unless[:sys][:network][:vlan_bridges] = Array.new
---
+++ definitions/sys_network_vlan_bridge.rb
@@ -0,0 +1,38 @@
+define :sys_network_vlan_bridge, :interface => 'eth0' do
+  vlanmap = Hash.new
+  vlanmap["UNTAGGED"]         =   0
+  vlanmap["DMZ"]              =   7
+  vlanmap["DMZ-SERVICE"]      =   8
+  vlanmap["INTERNESERVER1"]   = 104
+  vlanmap["INTERNESERVER3"]   = 118
+  vlanmap["DMZ-PROXY"]        = 119
+  vlanmap["INTERNESERVER2"]   = 124
+  vlanmap["DMZ-ACCESS-LINUX"] = 125
+
+  vlan_id = params[:name].kind_of?(String) ? vlanmap[params[:name]] : params[:name]
+
+  if vlan_id
+    interface = params[:interface]
+    vlan_interface = "#{interface}.#{vlan_id}"
+    bridge = "br#{vlan_id}"
+    if vlan_id == "0"
+      node.default.sys.network.interfaces[bridge] = {
+        "bridge_ports" => interface
+      }
+    else
+      node.default.sys.network.interfaces[vlan_interface] = {
+        "vlan-raw-device" => interface,
+        "up" => "ifup #{bridge}"
+      }
+      node.default.sys.network.interfaces[bridge] = {
+        "auto" => false,
+        "bridge_ports" => vlan_interface
+      }
+    end
+    # FAI is now creating this file in case of the default interface,
+    # so let's not touch this file for more resilience against chef screw-ups.
+    unless interface == node.network.default_interface
+      node.default.sys.network.interfaces[interface] = { "inet" => "static" }
+    end
+  end
+end
