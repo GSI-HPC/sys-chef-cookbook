7ce08237004b852c3c7af78efc64b58ef0ebf2c7
Dennis Klein <d.klein@gsi.de>
2014-03-24 15:23:20 +0100

    add a list of human readable service properties
    the idea is to make the motd more useful. It should
    contain a descriptive name as sys.banner.message at least.
    Additionally, you can add a list of human readable
    properties via sys.banner.service_properties (Array of Strings).

--- attributes/banner.rb
+++ attributes/banner.rb
@@ -1,4 +1,5 @@
 default_unless[:sys][:banner][:header] = String.new
 default_unless[:sys][:banner][:message] = String.new
+default_unless[:sys][:banner][:service_properties] = Array.new
 default_unless[:sys][:banner][:footer] = String.new
 default_unless[:sys][:banner][:info] = false
