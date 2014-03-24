011ab553a2b4df58192c164d1c82bdaeb60c62db
Dennis Klein <d.klein@gsi.de>
2014-03-24 15:26:38 +0100

    add test coverage

--- spec/unit/recipes/banner_spec.rb
+++ spec/unit/recipes/banner_spec.rb
@@ -38,7 +38,20 @@ describe 'sys::banner' do
       expect(chef_run).to create_template('/etc/motd').with_variables(
         :header => chef_run.node.sys.banner.header,
         :message => @expected_message,
+        :service_properties => chef_run.node.sys.banner.service_properties,
         :footer => chef_run.node.sys.banner.footer)
+    end
+  end
+
+  context 'with service_properties' do
+    before do
+      chef_run.node.default['sys']['banner']['message'] = 'asdf'
+      chef_run.node.default['sys']['banner']['service_properties'] = [ 'prop1', 'prop2' ]
+      chef_run.converge(described_recipe)
+    end
+
+    it 'renders a list of service_properties' do
+      expect(chef_run).to render_file('/etc/motd').with_content(/ \* prop1\n \* prop2\n/)
     end
   end
 end
