859c70a1d6642810be4773c24e2ab01911905f69
Matthias Pausch <user@localhost>
2014-04-09 14:03:32 +0200

    ensure that rsyslog is installed before starting it

--- recipes/rsyslog.rb
+++ recipes/rsyslog.rb
@@ -3,14 +3,16 @@
 #
 
 if node[:rsyslog].has_key?('server_ip')
-  
+
+  package "rsyslog"
+
   loghost_line = "*.*\t@"
   loghost_line += '@' if node['rsyslog']['protocol'] == 'tcp'
   loghost_line += node[:rsyslog][:server_ip]
   if node['rsyslog'].has_key?('port')
     loghost_line += node['rsyslog']['port'] unless node['rsyslog']['port'] == 514
   end
-  
+
   # forward everything to the loghost:
   file '/etc/rsyslog.d/loghost.conf' do
     content  "#{loghost_line}\n"
