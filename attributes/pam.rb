3e5b97fd98407875aaf8f7cdd0889d587ba99da8
Matthias Pausch <m.pausch@gsi.de>
2014-05-19 17:32:12 +0200

    Add deployment-code for /etc/pam.d/common-* via pam-update

--- attributes/pam.rb
+++ attributes/pam.rb
@@ -1,4 +1,5 @@
 default_unless[:sys][:pam][:group] = Array.new
 default_unless[:sys][:pam][:limits] = Array.new
 default_unless[:sys][:pam][:access] = Array.new
+default_unless[:sys][:pamupdate] = Hash.new
 default_unless[:sys][:pamd] = Hash.new
