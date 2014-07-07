ace4cdb5e263613a81288c1fd11439f95d7100b5
Matteo Dessalvi <m.dessalvi@gsi.de>
2014-07-07 14:52:03 +0200

    Included the variables attribute into the template definition

--- recipes/rsyslog.rb
+++ recipes/rsyslog.rb
@@ -24,6 +24,13 @@ if node[:rsyslog].has_key?('server_ip') and !node[:rsyslog][:server_ip].nil?
   # 2) all the mail.* event log will be directed only on /var/log/mail.*
   template '/etc/rsyslog.conf' do
     source 'etc_rsyslog.conf.erb'
+    owner "root"
+    group "root"
+    mode "0644"
+    variables(
+	      :ratelimit_burst => node.sys.rsyslog.ratelimit_burst, 
+	      :ratelimit_interval => node.sys.rsyslog.ratelimit_interval
+    )
     notifies :restart, "service[rsyslog]"
   end
 
