bdcef8c799e49acbbdc96d48964a4c2ac20a4740
Thomas Roth <t.roth@gsi.de>
2014-12-09 14:29:43 +0100

    Replace reboot upon update-grub by creation of /var/run/reboot-required on Debian

--- recipes/boot.rb
+++ recipes/boot.rb
@@ -25,11 +25,20 @@ unless node.sys.boot.params.empty? and node.sys.boot.config.empty?
     action :nothing
   end
 
+
   update_grub = 'Updating Grub boot configuration'
   execute update_grub  do
     action :nothing
     command 'update-grub2'
-    notifies :reboot, "sys_shutdown[now]", :immediately
+#    notifies :reboot, "sys_shutdown[now]", :immediately
+    if platform_family?('debian')
+      file "/var/run/reboot-required" do
+        owner 'root'
+        group 'root'
+        mode '0755'
+        action :create
+      end
+    end
   end
 
   template '/etc/default/grub' do
