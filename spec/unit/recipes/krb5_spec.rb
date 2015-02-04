8c45ba15eae820c0fb57d7b579aba06326796bb3
Matthias Pausch <user@localhost>
2015-02-04 15:29:49 +0100

    More or less a rewrite of krb5_spec.rb

--- spec/unit/recipes/krb5_spec.rb
+++ spec/unit/recipes/krb5_spec.rb
@@ -11,39 +11,26 @@ describe 'sys::krb5' do
   end
 
   context 'with basic attributes' do
-    before do
-      master = 'master.example.com'
-      slave = 'slave.example.com'
-      fqdn = 'node.example.com'
-      ktutil_command = "ktutil -k /etc/krb5.conf list --keys | grep "
-      ktutil_command += "-q host/node.example.com@EXAMPLE.COM"
-      stub_command(ktutil_command).and_return(0)
-      chef_run.node.default['sys']['krb5']['realm'] = 'example.com'
-      chef_run.node.default['sys']['krb5']['master'] = master
-      chef_run.node.default['sys']['krb5']['admin_server'] = master
-      chef_run.node.default['sys']['krb5']['slave'] = slave
-      chef_run.node.default['sys']['krb5']['distribution'] = 'wallet'
-      chef_run.node.default['sys']['krb5']['keytab_config'] = [
-        { :keytab => "host",
-          :place => "/etc/krb5.conf" }
-      ]
-      chef_run.node.automatic['fqdn'] = fqdn
-      chef_run.node.automatic['domain'] = "example.com"
-      chef_run.converge(described_recipe)
-    end
 
-    it 'manages /etc/krb5.conf' do
-      expect(chef_run).to create_template('/etc/krb5.conf').with_mode('0644')
+    before do
+      chef_run.node.default.sys.krb5.realm = "example.com"
+      chef_run.node.default['sys']['krb5']['admin_server'] = "master.example.com"
+      chef_run.node.default['sys']['krb5']['master'] = "master.example.com"
+      chef_run.node.default['sys']['krb5']['slave'] = "slave.example.com"
+      chef_run.node.automatic['domain'] = 'example.com'
+      chef_run.converge(described_recipe)
     end
 
     it 'installs packages' do
       expect(chef_run).to install_package('heimdal-clients')
       expect(chef_run).to install_package('libpam-heimdal')
       expect(chef_run).to install_package('heimdal-kcm')
+      expect(chef_run).to install_package('heimdal-docs')
       expect(chef_run).to install_package('libsasl2-modules-gssapi-heimdal')
     end
 
-    it 'defines attributes in /etc/krb5.conf' do
+    it 'manages /etc/krb5.conf' do
+      expect(chef_run).to create_template('/etc/krb5.conf').with_mode('0644')
       expect(chef_run).to create_template('/etc/krb5.conf').with(
         :variables => {
           :realm => "EXAMPLE.COM",
@@ -54,44 +41,27 @@ describe 'sys::krb5' do
           :use_pkinit => nil
         }
       )
-
-      test = chef_run.find_resource(:template, '/etc/krb5.conf')
-      expect(ChefSpec::Renderer.new(chef_run, test).content).to_not include("try_pkinit =")
-      expect(ChefSpec::Renderer.new(chef_run, test).content).to_not include("wallet_server =")
-      expect(chef_run).to render_file('/etc/krb5.conf').with_content(
-        "\texample.com = EXAMPLE.COM\n" +
-          "\t.example.com = EXAMPLE.COM"
+      expect(chef_run).to render_file("/etc/krb5.conf").with_content(
+        "\t\tkdc = master.example.com\n\t\tkdc = slave.example.com"
       )
-
       expect(chef_run).to render_file('/etc/krb5.conf').with_content(
-        "\t\tkdc = master.example.com\n" +
-          "\t\tkdc = slave.example.com"
+        "\texample.com = EXAMPLE.COM\n\t.example.com = EXAMPLE.COM"
       )
     end
   end
 
   context 'with wallet and pkinit attributes' do
+
     before do
-      master = 'master.example.com'
-      slave = 'slave.example.com'
-      fqdn = 'node.example.com'
-      ktutil_command = "ktutil -k /etc/krb5.conf list --keys | grep "
-      ktutil_command += "-q host/node.example.com@EXAMPLE.COM"
-      stub_command(ktutil_command).and_return(0)
-      chef_run.node.default['sys']['krb5']['keytab_config'] = [
-        { :keytab => "host",
-          :place => "/etc/krb5.conf" }
-      ]
-      chef_run.node.default['sys']['krb5']['realm'] = 'example.com'
-      chef_run.node.default['sys']['krb5']['master'] = master
-      chef_run.node.default['sys']['krb5']['admin_server'] = master
-      chef_run.node.default['sys']['krb5']['slave'] = slave
-      chef_run.node.default['sys']['krb5']['wallet_server'] = 'wallet.example.com'
-      chef_run.node.default['sys']['krb5']['distribution'] = 'wallet'
+      chef_run.node.default.sys.krb5.realm = "example.com"
+      chef_run.node.default['sys']['krb5']['admin_server'] = "master.example.com"
+      chef_run.node.default['sys']['krb5']['master'] = "master.example.com"
+      chef_run.node.default['sys']['krb5']['slave'] = "slave.example.com"
       chef_run.node.default['sys']['krb5']['use_pkinit'] = true
-      chef_run.node.automatic['fqdn'] = fqdn
-      chef_run.node.automatic['domain'] = "example.com"
+      chef_run.node.default['sys']['krb5']['wallet_server'] = "wallet.example.com"
+      chef_run.node.automatic['domain'] = 'example.com'
       chef_run.converge(described_recipe)
+      stub_command('File.exists?("/etc/krb5.keytab"').and_return(true)
     end
 
     it "configures pkinit and wallet" do
@@ -105,14 +75,77 @@ describe 'sys::krb5' do
           :use_pkinit => true
         }
       )
+      expect(chef_run).to render_file("/etc/krb5.conf").with_content("\tuse_pkinit = true")
+      expect(chef_run).to render_file('/etc/krb5.conf').with_content("\t\twallet_server = wallet.example.com")
 
-      expect(chef_run).to render_file('/etc/krb5.conf').with_content(
-        "\tuse_pkinit = true"
-      )
+    end
+  end
 
-      expect(chef_run).to render_file('/etc/krb5.conf').with_content(
-        "\t\twallet_server = wallet.example.com"
+  context 'with keytab_config and distribution wallet' do
+
+    before do
+      chef_run.node.default['sys']['krb5']['distribution'] = "wallet"
+      chef_run.node.default['sys']['krb5'][:'keytab_config'] = [
+        { :keytab => "default" },
+        { :keytab => "foo",
+          :place => "/etc/bar.keytab",
+          :mode => "0666",
+          :owner => "foo",
+          :group => "bar" },
+        { :keytab => "not_there",
+          :place => "/does/not/exist"}
+      ]
+      chef_run.node.automatic['domain'] = 'example.com'
+      chef_run.node.automatic['fqdn'] = 'node.example.com'
+      chef_run.node.default.sys.krb5.realm = "example.com"
+      ktutil_command = "ktutil -k /etc/default.keytab list --keys | grep "
+      ktutil_command += "-q default/node.example.com@EXAMPLE.COM"
+      stub_command(ktutil_command).and_return(true)
+      ktutil2_command = "ktutil -k /etc/bar.keytab list --keys | grep "
+      ktutil2_command += "-q foo/node.example.com@EXAMPLE.COM"
+      stub_command(ktutil2_command).and_return(true)
+      ktutil3_command = "ktutil -k /does/not/exist list --keys | grep "
+      ktutil3_command += "-q not_there/node.example.com@EXAMPLE.COM"
+      stub_command(ktutil3_command).and_return(false)
+
+      allow(File).to receive(:exists?).and_call_original
+      allow(File).to receive(:exists?).with('/etc/default.keytab').and_return(true)
+      allow(File).to receive(:exists?).with('/etc/bar.keytab').and_return(true)
+      allow(File).to receive(:exists?).with('/does/not/exist').and_return(false)
+      chef_run.converge(described_recipe)
+    end
+
+    it 'installs wallet-client' do
+      expect(chef_run).to install_package('wallet-client')
+    end
+
+    it 'sets default-mode for keytabs' do
+      expect(chef_run).to create_file('/etc/default.keytab').with(
+        :user => "root",
+        :group => "root",
+        :mode => "0600"
       )
+    end
+
+    it 'sets given mode for keytabs' do
+      expect(chef_run).to create_file('/etc/bar.keytab').with(
+        :user => "foo",
+        :group => "bar",
+        :mode => "0666"
+      )
+    end
+
+    it 'does not create keytab-files' do
+      expect(chef_run).not_to create_file("/does/not/exist")
+    end
+
+    it 'does deploy missing keytabs' do
+      expect(chef_run).to run_bash('deploy not_there/node.example.com@EXAMPLE.COM')
+    end
+
+    it 'does not deploy existing keytabs' do
+      expect(chef_run).not_to run_bash('deploy default/node.example.com@EXAMPLE.COM')
+      expect(chef_run).not_to run_bash('deploy foo/node.example.com@EXAMPLE.COM')
     end
   end
 end
