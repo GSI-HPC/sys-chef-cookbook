e9d43b0139907342da2e2ff24827448650709f0b
Matthias Pausch <user@localhost>
2015-02-04 15:30:30 +0100

    Reorganize accounts_spec.rb

--- spec/unit/recipes/accounts_spec.rb
+++ spec/unit/recipes/accounts_spec.rb
@@ -1,5 +1,8 @@
 describe 'sys::accounts' do
-  let(:chef_run) { ChefSpec::Runner.new().converge(described_recipe) }
+  let(:chef_run) do
+    ChefSpec::SoloRunner.new do |node|
+    end.converge(described_recipe)
+  end
 
   context 'node.sys.accounts is empty' do
     it 'does nothing' do
@@ -8,11 +11,24 @@ describe 'sys::accounts' do
   end
 
   context 'with some test attributes' do
-    before do
+
+    it 'installs package ruby-shadow' do
+      chef_run.node.default['sys']['accounts'] = { :not => "empty" }
+      chef_run.converge(described_recipe)
+      expect(chef_run).to install_package('ruby-shadow')
+    end
+
+    it 'manages groups' do
       @g1 = { 'gid' => 1337 }
       @g2 = {}
       chef_run.node.default['sys']['groups']['g1'] = @g1
       chef_run.node.default['sys']['groups']['g2'] = @g2
+      chef_run.converge(described_recipe)
+      expect(chef_run).to create_group('g1').with_gid(1337)
+      expect(chef_run).to create_group('g2')
+    end
+
+    it 'manages users' do
       @u1 = {
         'uid'      => 666,
         'gid'      => 'fauxhai',
@@ -24,24 +40,11 @@ describe 'sys::accounts' do
         'supports' => { 'manage_home' => true }
       }
       @u2 = { 'gid' => 0 } # the fauxhai group has gid 0
-      @u3 = { 'supports' => { 'manage_home' => true } }
       @u4 = { 'gid' => 'doesnotexist' }
       chef_run.node.default['sys']['accounts']['u1'] = @u1
       chef_run.node.default['sys']['accounts']['u2'] = @u2
-      chef_run.node.default['sys']['accounts']['u3'] = @u3
       chef_run.node.default['sys']['accounts']['u4'] = @u4
-    end
-
-    it 'installs package ruby-shadow' do
-      expect(chef_run).to install_package('ruby-shadow')
-    end
-
-    it 'manages groups' do
-      expect(chef_run).to create_group('g1').with_gid(1337)
-      expect(chef_run).to create_group('g2')
-    end
-
-    it 'manages users' do
+      chef_run.converge(described_recipe)
       expect(chef_run).to create_user('u1').with({
         :uid => @u1['uid'],
         :gid => @u1['gid'],
@@ -53,11 +56,13 @@ describe 'sys::accounts' do
         :system => @u1['system']
       })
       expect(chef_run).to create_user('u2')
-      expect(chef_run).to create_user('u3')
       expect(chef_run).to_not create_user('u4')
     end
 
     it 'honors manage_home flag' do
+      @u3 = { 'supports' => { 'manage_home' => true } }
+      chef_run.node.default['sys']['accounts']['u3'] = @u3
+      chef_run.converge(described_recipe)
       expect(chef_run).to create_user('u3').with_supports(@u3['supports'])
     end
 
@@ -65,13 +70,11 @@ describe 'sys::accounts' do
 end
 
 describe 'sys::accounts_db' do
-  let(:chef_run) { ChefSpec::Runner.new(mode: :server).converge(described_recipe) }
-
-  context 'with accounts data bag' do
-    before do
-      chef_run.node.default['sys']['accounts']['v1'] = { }
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new do |node, server|
+      node.default['sys']['accounts']['v1'] = { }
       @v2 = { 'gid' => 1337 }
-      chef_run.node.default['sys']['accounts']['v2'] = @v2
+      node.default['sys']['accounts']['v2'] = @v2
 
       @v1item = {
         'id' => 'v1',
@@ -87,13 +90,15 @@ describe 'sys::accounts_db' do
         }
       }
 
-      ChefSpec::Server.create_data_bag('accounts', {
-          'v1' => @v1item,
-          'v2' => @v2item
-        })
-      ChefSpec::Server.create_data_bag('localgroups', { })
-      chef_run.converge(described_recipe)
-    end
+      server.create_data_bag('accounts', {
+        'v1' => @v1item,
+        'v2' => @v2item
+      })
+      server.create_data_bag('localgroups', { })
+    end.converge(described_recipe)
+  end
+
+  context 'with accounts data bag' do
 
     it 'manages users from data bags' do
       expect(chef_run).to create_user('v2').with_home(@v2item['account']['home']).with_gid(@v2['gid'])
