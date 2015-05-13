0e973d558615144e2feea2de3392996721f9f0e4
Dennis Klein <d.klein@gsi.de>
2015-05-13 14:56:07 +0200

    update pci ohai plugin to new syntax

--- files/default/ohai_plugins/pci.rb
+++ files/default/ohai_plugins/pci.rb
@@ -1,11 +1,16 @@
-provides 'pci'
-pci Array.new
-`lspci -m`.split("\n").each do |line|
-  line = line.split(' "').map { |e| e.delete('"') }
-  pci << {
-    'slot' => line[0],
-    'type' => line[1],
-    'vendor' => line[2],
-    'device' => line[3][0..-6] # cut the revision number
-  }
+Ohai.plugin(:Pci) do
+  provides 'pci'
+
+  collect_data(:default) do
+    pci Array.new
+    `lspci -m`.split("\n").each do |line|
+       line = line.split(' "').map { |e| e.delete('"') }
+       pci << {
+         'slot' => line[0],
+         'type' => line[1],
+         'vendor' => line[2],
+         'device' => line[3][0..-6] # cut the revision number
+       }
+    end
+  end
 end
