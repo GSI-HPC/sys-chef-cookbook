5331b2f167c5945578992bbeea02ece834f4208d
Dennis Klein <d.klein@gsi.de>
2015-06-11 17:49:13 +0200

    flag which writes current interface config into systemd network unit.
    refs #729

--- attributes/systemd.rb
+++ attributes/systemd.rb
@@ -2,4 +2,5 @@ default_unless['sys']['systemd'] = Mash.new
 default_unless['sys']['systemd']['networkd'] = Mash.new
 default_unless['sys']['systemd']['networkd']['enable'] = false
 default_unless['sys']['systemd']['networkd']['clean_legacy'] = false
+default_unless['sys']['systemd']['networkd']['make_primary_interface_persistent'] = false
 default_unless['sys']['systemd']['unit'] = Mash.new
