68013b23d93365ef88d60b1a00bf7e1fd2213db3
Christopher Huhn <C.Huhn@gsi.de>
2015-07-09 13:53:36 +0200

    Basic IPMI recipe

---
+++ recipes/ipmi.rb
@@ -0,0 +1,9 @@
+# 
+# stuff to set up for IPMI BMC configuration
+#
+
+# the simple tool:
+package 'ipmitool'
+
+# the powerful tools (used by the supplied IPMI ohai plugin):
+package 'freeipmi-tools'
