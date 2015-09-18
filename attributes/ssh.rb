94535239c68086a8d183311cef711556c23133d4
Matthias Pausch <m.pausch@gsi.de>
2015-09-18 11:48:08 +0200

    Add support system-wide ssh client-config

--- attributes/ssh.rb
+++ attributes/ssh.rb
@@ -1,3 +1,4 @@
 default_unless['sys']['sshd']['config'] = Hash.new
+default_unless['sys']['ssh']['ssh_config'] = Hash.new
 default_unless['sys']['ssh']['authorize'] = Mash.new
 default_unless['sys']['ssh']['config'] = Mash.new
