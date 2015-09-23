fda6b121c58bdfae0415ea077784cebea17fecfc
Christopher Huhn <C.Huhn@gsi.de>
2015-09-23 14:20:50 +0200

    Add logic to create SNMPv3 users

--- recipes/snmp.rb
+++ recipes/snmp.rb
@@ -20,9 +20,9 @@
 
 
 if node['sys']['snmp']
-  
+
   package 'snmpd'
-  
+
   template '/etc/snmp/snmpd.conf' do
     mode 0600
     source 'etc_snmp_snmpd.conf.erb'
@@ -33,17 +33,44 @@ if node['sys']['snmp']
         :community     => node['sys']['snmp']['community'],
         :extensions    => node['sys']['snmp']['extensions'] || [],
         :full_access   => node['sys']['snmp']['full_access'],
-        :sys_contact   => node['sys']['snmp']['sys_contact'] || "Sysadmins <root@#{node['fqdn']}>",
+        :sys_contact   => node['sys']['snmp']['sys_contact'] ||
+                          "Sysadmins <root@#{node['fqdn']}>",
         :sys_location  => node['sys']['snmp']['sys_location']
       })
   end
 
-  # TODO: for now the defaultMonitors are turned off, they produce bogus error messages on service startup
-  #       unless MIBs are available (package 'snmp-mibs-downloader') 
+  # TODO: for now the defaultMonitors are turned off, they produce bogus error
+  #       messages  service startup  MIBs are available
+  #       (package 'snmp-mibs-downloader')
   #       and configured in /etc/default/snmpd ('export MIBS=/usr/share/mibs')
 
   service 'snmpd' do
     action [:enable, :start]
   end
-  
+
+  # add SNMPv3 users:
+  if node['sys']['snmp']['snmpv3_users']
+
+    package 'libsnmp-dev' do
+      # In Jessie net-snmp-config moved to this package:
+      only_if { node['platform_version'].to_f >= 8.0 }
+    end
+
+    # snmpd must not run during net-snmp-config
+    service 'stop_snmpd' do
+      service_name 'snmpd'
+      action :stop
+    end
+
+    node['sys']['snmp']['snmpv3_users'].each do |u|
+      execute "create_snmpv3_user_#{u['securityName']}" do
+        command "net-snmp-config --create-snmpv3-user" +
+                " #{'-ro' unless u['readonly'] == false} -A #{u['authKey']} " +
+                " -X #{u['privKey']} -a #{u['authProtocol'] || 'SHA'}" +
+                " -x #{u['privProtocol'] || 'AES'} #{u['securityName']}"
+        notifies :start, "service[snmpd]", :delayed
+      end
+    end
+  end
+
 end
