062a8bc3c1d5ae72f1c417b772fafc1133d32d7d
Victor Penso <v.penso@gsi.de>
2015-12-18 13:15:02 +0100

    Check for the init system to deploy the a cron job to restart chef-client

--- tests/roles/sys_chef_test.rb
+++ tests/roles/sys_chef_test.rb
@@ -2,12 +2,13 @@ name "sys_chef_test"
 description "Use to test the [sys::chef] recipe."
 run_list( "recipe[sys::chef]" )
 default_attributes(
-  :sys => {
-    :chef => {
-      :server_url => "http://chef.devops.test:443"
+  sys: {
+    chef: {
+      server_url: "http://chef.devops.test:443",
+      restart_via_cron: true
     }
   },
-  :ohai => {
-    :disabled_plugins => [ :Passwd, :Filesystem, 'etc' ]
+  ohai: {
+    disabled_plugins: [ :Passwd, :Filesystem, 'etc' ]
   }
 )
