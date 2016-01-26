517fbf0de49b42a4bf1f2220d79abc34f175cc9d
Dennis Klein <d.klein@gsi.de>
2016-01-26 18:18:42 +0100

    foodcritic maintenance

--- attributes/updatedb.rb
+++ attributes/updatedb.rb
@@ -1,3 +1,3 @@
-default_unless[:sys][:updatedb][:prunebindmounts] = String.new
-default_unless[:sys][:updatedb][:prunepaths] = String.new
-default_unless[:sys][:updatedb][:prunefs] = String.new
+default_unless['sys']['updatedb']['prunebindmounts'] = String.new
+default_unless['sys']['updatedb']['prunepaths'] = String.new
+default_unless['sys']['updatedb']['prunefs'] = String.new
--- recipes/updatedb.rb
+++ recipes/updatedb.rb
@@ -2,17 +2,16 @@
 # Copy a sensible /etc/updatedb.conf to every host
 #
 
-unless node[:sys][:updatedb].empty? 
+unless node['sys']['updatedb'].empty? # ~FC023
   template '/etc/updatedb.conf' do
     source 'etc_updatedb.conf.erb'
     owner "root"
     group "root"
     mode "0600"
     variables({
-                :prunebindmounts => node[:sys][:updatedb][:prunebindmounts],
-                :prunepaths      => node[:sys][:updatedb][:prunepaths],
-                :prunefs         => node[:sys][:updatedb][:prunefs]
-              })
+      :prunebindmounts => node['sys']['updatedb']['prunebindmounts'],
+      :prunepaths      => node['sys']['updatedb']['prunepaths'],
+      :prunefs         => node['sys']['updatedb']['prunefs']
+    })
   end
 end
-  
