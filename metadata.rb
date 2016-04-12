7ef0adb61f31fa23975e30757f97b17da1205168
Christopher Huhn <C.Huhn@gsi.de>
2016-04-12 18:02:16 +0200

    Add source and issues url

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'Apache 2.0'
 description      'System Software configuration and maintenance'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.14.2'
+source_url       'https://github.com/GSI-HPC/sys-chef-cookbook'
+issues_url       'https://github.com/GSI-HPC/sys-chef-cookbook/issues'
