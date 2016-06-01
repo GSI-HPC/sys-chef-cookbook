41651891f19b5c98e40ba523bff880f4e8a08602
Christopher Huhn <C.Huhn@gsi.de>
2016-06-01 14:32:11 +0200

    Add smartd recipe

---
+++ recipes/smartd.rb
@@ -0,0 +1,34 @@
+#
+# install smartmontools and configure smartd
+#
+
+enable = node['sys']['smartd']['enable']
+
+if enable && node['virtualization']['role'] == 'guest'
+  enable = false
+  log 'According to Ohai this is a VM - not enabling smartd'
+end
+
+if enable
+  package 'smartmontools'
+
+  service 'smartmontools' do
+    action :enable, :start
+  end
+
+  template '/etc/default/smartmontools' do
+    source 'etc_default_smartmontools.erb'
+    variables(
+      enable: enable
+    )
+    notifies :restart, 'service[smartmontools]'
+  end
+
+  template '/etc/smartd.conf' do
+    source 'etc_smartd.conf.erb'
+    variables(
+      mailto: node['sys']['smartd']['mailto']
+    )
+    notifies :reload, 'service[smartmontools]'
+  end
+end
