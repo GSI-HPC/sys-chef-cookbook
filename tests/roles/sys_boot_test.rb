c063393e8d8dd444be69270a8f8eadbf444221fa
Victor Penso <v.penso@gsi.de>
2016-06-21 09:35:40 +0200

    Add default Grub configuration file

--- tests/roles/sys_boot_test.rb
+++ tests/roles/sys_boot_test.rb
@@ -1,13 +1,13 @@
-name "sys_boot_test"
-description "Use to test the [sys::boot] recipe."
-run_list( "recipe[sys]" )
+name 'sys_boot_test'
+description 'Use to test the [sys::boot] recipe.'
+run_list( 'recipe[sys::boot]' )
 default_attributes(
-  "sys" => {
-    "boot" => {
-      "params" => [
-        "earlyprintk",
-        "selinux=0",
-        "panic=10"
+  sys: {
+    boot: {
+      params: [
+        'earlyprintk',
+        'selinux=0',
+        'panic=10'
       ]
     }
   }
