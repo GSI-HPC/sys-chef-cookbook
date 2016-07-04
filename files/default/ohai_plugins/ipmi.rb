eeb93c58e77ffd87d54f64d8a6f3b4e24a407083
Christopher Huhn <C.Huhn@gsi.de>
2016-07-04 17:23:06 +0200

    Fix rubocop complaints about unused variables

--- files/default/ohai_plugins/ipmi.rb
+++ files/default/ohai_plugins/ipmi.rb
@@ -31,7 +31,7 @@ Ohai.plugin(:Ipmi) do
       ipmi Mash.new
       ipmi['bmc-config'] = bmc_config
       ipmi['pef-config'] = pef_config
-      ipmi['sensors-config'] = ipmi_config('ipmi-sensors').delete_if{ |k,v|
+      ipmi['sensors-config'] = ipmi_config('ipmi-sensors').delete_if{ |_k,v|
         v.empty?
       }
     end
@@ -99,7 +99,7 @@ Ohai.plugin(:Ipmi) do
 
   def bmc_config()
     # skip disabled users:
-    ipmi_config.delete_if{|k,v| v['Enable_User'] == 'No'}
+    ipmi_config.delete_if{|_k,v| v['Enable_User'] == 'No'}
   end
 
   def pef_config()
@@ -114,7 +114,7 @@ Ohai.plugin(:Ipmi) do
 
     # TODO: aad alert destinations and strings that aren't mentioned in any
     #       event filter definition
-    return h.select{ |k,v|
+    return h.select{ |k,_v|
       k == 'PEF_Conf' or k == 'Community_String'
     }.merge(event_filters)
   end
