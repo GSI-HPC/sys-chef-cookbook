86e19e43ee45b016f238a44889263d444c0e28d1
Christopher Huhn <C.Huhn@gsi.de>
2016-07-14 15:23:24 +0200

    use_inline_resources automatically (and correctly) sets updated_by_last_action for LWRPs
    Conflicts:
    	metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,6 @@ maintainer_email 'hpc@gsi.de'
 license          'Apache 2.0'
 description      'System Software configuration and maintenance'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.21.0'
+source_url       'https://github.com/GSI-HPC/sys-chef-cookbook'
+issues_url       'https://github.com/GSI-HPC/sys-chef-cookbook/issues'
+version          '1.21.1'
