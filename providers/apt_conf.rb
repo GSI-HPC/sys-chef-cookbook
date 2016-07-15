0ec14b6a7b506b09dd657723da37ba8b65bde343
Christopher Huhn <C.Huhn@gsi.de>
2016-07-15 10:47:31 +0200

    Also fix sys_apt_conf LWRP's updated_by_last_action logic

--- providers/apt_conf.rb
+++ providers/apt_conf.rb
@@ -40,7 +40,8 @@ action :set do
     notifies :run, "execute[#{update}]", :immediately
   end
 
-  new_resource.updated_by_last_action(true)
+  # superseded by use_inline_resources
+  #new_resource.updated_by_last_action(true)
 
 end
 
@@ -60,6 +61,7 @@ action :remove do
     notifies :run, "execute[#{update}]", :immediately
   end
 
-  new_resource.updated_by_last_action(true)
+  # superseded by use_inline_resources
+  #new_resource.updated_by_last_action(true)
 
 end
