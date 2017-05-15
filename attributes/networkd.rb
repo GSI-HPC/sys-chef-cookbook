b83e289bacb76afa01f8dca4299bb3b0ada33b8b
Matthias Pausch <m.pausch@gsi.de>
2017-05-15 14:31:26 +0200

    Configure network via systemd-networkd

---
+++ attributes/networkd.rb
@@ -0,0 +1,1 @@
+node['sys']['networkd'] = {}
--- recipes/network.rb
+++ recipes/network.rb
@@ -16,76 +16,78 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+if node['platform_version'].to_i < 9
 
-unless node['sys']['network']['vlan_bridges'].empty?
-  node['sys']['network']['vlan_bridges'].each do |bridge|
-    sys_network_vlan_bridge bridge do
-      interface node['network']['default_interface']
-    end
-  end
-end
-
-interfaces = node['sys']['network']['interfaces']
-unless interfaces.empty?
-
-  # FIXME: we only need these packages if we actually mess with VLANs
-  package 'vlan'
-  package 'bridge-utils'
-
-  service 'networking'
-
-  directory '/etc/network/interfaces.d'
-
-  if node['sys']['network']['keep_interfaces']
-    cmd = Mixlib::ShellOut.new('grep -q "^source /etc/network/interfaces\.d/*" /etc/network/interfaces')
-    cmd.run_command
-    if cmd.status != 0
-      File.open("/etc/network/interfaces", "a") do |intf|
-        intf.puts "\n#added by Chef:\nsource /etc/network/interfaces.d/*"
+  unless node['sys']['network']['vlan_bridges'].empty?
+    node['sys']['network']['vlan_bridges'].each do |bridge|
+      sys_network_vlan_bridge bridge do
+        interface node['network']['default_interface']
       end
-    end
-  else
-    cookbook_file '/etc/network/interfaces' do
-      source 'etc_network_interfaces'
     end
   end
 
-  interfaces.each do |name,params|
+  interfaces = node['sys']['network']['interfaces']
+  unless interfaces.empty?
 
-    # set defaults
-    inet = params.has_key?(:inet) ? params[:inet] : 'manual'
-    auto = params.has_key?(:auto) ? params[:auto] : true
+    # FIXME: we only need these packages if we actually mess with VLANs
+    package 'vlan'
+    package 'bridge-utils'
 
-    # merge the configuration
-    config = Hash.new
-    params.each do |key,value|
-      unless ["inet", "auto"].include? key
-        config[key] = value
+    service 'networking'
+
+    directory '/etc/network/interfaces.d'
+
+    if node['sys']['network']['keep_interfaces']
+      cmd = Mixlib::ShellOut.new('grep -q "^source /etc/network/interfaces\.d/*" /etc/network/interfaces')
+      cmd.run_command
+      if cmd.status != 0
+        File.open("/etc/network/interfaces", "a") do |intf|
+          intf.puts "\n#added by Chef:\nsource /etc/network/interfaces.d/*"
+        end
+      end
+    else
+      cookbook_file '/etc/network/interfaces' do
+        source 'etc_network_interfaces'
       end
     end
 
-    # try to get configuration of the default interface from Ohai
-    if name == node['network']['default_interface'] and inet == 'static'
-      config[:address] = node['ipaddress'] unless config.has_key?(:address)
-      config[:gateway] = node['network']['default_gateway'] unless config.has_key?(:gateway)
-      config[:broadcast] = node['network']['interfaces'][node['network']['default_interface']]['addresses'][config[:address]]['broadcast'] unless config.has_key?(:broadcast)
-      config[:netmask] = node['network']['interfaces'][node['network']['default_interface']]['addresses'][config[:address]]['netmask'] unless config.has_key?(:netmask)
-    end
+    interfaces.each do |name,params|
 
-    template "/etc/network/interfaces.d/#{name}" do
-      source 'etc_network_interfaces.d_generic.erb'
-      mode "0644"
-      variables(
-        :auto => auto,
-        :name => name,
-        :inet => inet,
-        :config => config
-      )
-      if node['sys']['network']['restart']
-        notifies :restart, 'service[networking]'
+      # set defaults
+      inet = params.has_key?(:inet) ? params[:inet] : 'manual'
+      auto = params.has_key?(:auto) ? params[:auto] : true
+
+      # merge the configuration
+      config = Hash.new
+      params.each do |key,value|
+        unless ["inet", "auto"].include? key
+          config[key] = value
+        end
       end
+
+      # try to get configuration of the default interface from Ohai
+      if name == node['network']['default_interface'] and inet == 'static'
+        config[:address] = node['ipaddress'] unless config.has_key?(:address)
+        config[:gateway] = node['network']['default_gateway'] unless config.has_key?(:gateway)
+        config[:broadcast] = node['network']['interfaces'][node['network']['default_interface']]['addresses'][config[:address]]['broadcast'] unless config.has_key?(:broadcast)
+        config[:netmask] = node['network']['interfaces'][node['network']['default_interface']]['addresses'][config[:address]]['netmask'] unless config.has_key?(:netmask)
+      end
+
+      template "/etc/network/interfaces.d/#{name}" do
+        source 'etc_network_interfaces.d_generic.erb'
+        mode "0644"
+        variables(
+          :auto => auto,
+          :name => name,
+          :inet => inet,
+          :config => config
+        )
+        if node['sys']['network']['restart']
+          notifies :restart, 'service[networking]'
+        end
+      end
+
     end
 
   end
-
 end
