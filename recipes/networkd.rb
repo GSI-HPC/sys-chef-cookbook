0d2911a48fa2f1825600a4094b22a1f8729d3164
Matthias Pausch <m.pausch@gsi.de>
2017-05-15 14:31:26 +0200

    Configure network via systemd-networkd

---
+++ recipes/networkd.rb
@@ -0,0 +1,56 @@
+#
+# Cookbook Name:: sys
+# Recipe:: networkd
+#
+# Copyright 2017, Matthia Pausch
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if node['platform_version'].to_i >= 9 && !node['sys']['networkd'].empty?
+
+  node['sys']['networkd']['rename'].each do |mac, name|
+    template "/etc/systemd/network/00-#{name}.link" do
+      source "systemd_networkd_generic.erb"
+      helpers(Sys::Harry)
+      mode "0644"
+      vars(:sections => {'Match' => {'MACAddress' => mac}, 'Link' => {'Name' => name}})
+    end
+  end
+
+  node['sys']['networkd']['netdev'].each do |name, config|
+    number_prefix = ''
+    unless name.match(/^[0-9]{2}-/)
+      number_prefix = '10-'
+    end
+    template "/etc/systemd/network/#{number_prefix}#{name}.netdev" do
+      source "systemd_networkd_generic.erb"
+      helpers(Sys::Harry)
+      mode "0644"
+      vars(:sections => config)
+    end
+  end
+
+  node['sys']['networkd']['network'].each do |name, config|
+    number_prefix = ''
+    unless name.match(/^[0-9]{2}-/)
+      number_prefix = '20-'
+    end
+    template "/etc/systemd/network/#{number_prefix}#{name}.network" do
+      source "systemd_networkd_generic.erb"
+      helpers(Sys::Harry)
+      mode "0644"
+      vars(:sections => config)
+    end
+  end
+end
