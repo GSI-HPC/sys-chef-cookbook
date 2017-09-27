a525ab20aa7838d8b2f46d56771cb00be46303b3
Christopher Huhn <c.huhn@gsi.de>
2017-09-27 12:14:11 +0200

    Fix small foodcritic complaints

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,12 @@
 name             'sys'
 maintainer       'GSI HPC department'
 maintainer_email 'hpc@gsi.de'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'System Software configuration and maintenance'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/GSI-HPC/sys-chef-cookbook'
 issues_url       'https://github.com/GSI-HPC/sys-chef-cookbook/issues'
 version          '1.29.5'
+
+chef_version     '>= 12.0' if respond_to?(:chef_version)
+supports         'debian'
