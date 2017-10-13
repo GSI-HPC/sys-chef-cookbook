f395fcc9cd4781ee707ee4e3bda721005ba92d71
Matthias Pausch <m.pausch@gsi.de>
2017-10-13 14:43:32 +0200

    Add namespace-support for sshd

--- recipes/pam.rb
+++ recipes/pam.rb
@@ -35,11 +35,20 @@ template '/etc/security/access.conf' do
   end
 end
 
+if node['platform_version'].to_i >= 9
+  cookbook_file '/etc/security/namespace.conf' do
+    source 'etc_security_namespace.conf'
+    owner 'root'
+    group 'root'
+    mode '0644'
+  end
+end
+
 #
 # PAM sshd config
 #
-cookbook_file '/etc/pam.d/sshd' do
-  source 'etc_pam.d_sshd'
+template '/etc/pam.d/sshd' do
+  source 'etc_pam.d_sshd.erb'
   owner 'root'
   group 'root'
   mode "0644"
