b7243c52e5847095d4f3545d9aba60176235ab5d
Matthias Pausch <m.pausch@gsi.de>
2017-10-24 13:45:16 +0200

    Fix krb5_spec.rb

--- test/unit/recipes/krb5_spec.rb
+++ test/unit/recipes/krb5_spec.rb
@@ -10,101 +10,11 @@ describe 'sys::krb5' do
     end
   end
 
-  context 'with basic attributes' do
-    cached(:chef_run) do
-      ChefSpec::SoloRunner.new do |node|
-        node.default['sys']['krb5']['realm'] = "example.com"
-        node.default['sys']['krb5']['admin_server'] = "master.example.com"
-        node.default['sys']['krb5']['master'] = "master.example.com"
-        node.default['sys']['krb5']['slave'] = "slave.example.com"
-        node.automatic['domain'] = 'example.com'
-        node.automatic['fqdn'] = 'node.example.com'
-      end.converge(described_recipe)
-    end
-
-    it 'installs packages' do
-      expect(chef_run).to install_package('heimdal-clients')
-      expect(chef_run).to install_package('libpam-heimdal')
-      expect(chef_run).to install_package('heimdal-kcm')
-      expect(chef_run).to install_package('heimdal-docs')
-      expect(chef_run).to install_package('libsasl2-modules-gssapi-heimdal')
-      expect(chef_run).to install_package('kstart')
-      expect(chef_run).to install_package('wallet-client')
-    end
-
-    it 'manages /etc/krb5.conf' do
-      expect(chef_run).to create_template('/etc/krb5.conf').with_mode('0644')
-      expect(chef_run).to create_template('/etc/krb5.conf').with(
-        :variables => {
-          :realm => "EXAMPLE.COM",
-          :admin_server => "master.example.com",
-          :servers => [ 'master.example.com', 'slave.example.com'],
-          :domain => "example.com",
-          :realms => [],
-          :wallet_server => nil,
-          :use_pkinit => nil,
-          :libdefaults => nil
-        }
-      )
-      expect(chef_run).to render_file("/etc/krb5.conf").with_content(
-        "\t\tkdc = master.example.com\n\t\tkdc = slave.example.com"
-      )
-      expect(chef_run).to render_file('/etc/krb5.conf').with_content(
-        "\texample.com = EXAMPLE.COM\n\t.example.com = EXAMPLE.COM"
-      )
-    end
-  end
-
-  context 'with wallet and pkinit attributes' do
-    cached(:chef_run) do
-      ChefSpec::SoloRunner.new do |node|
-        node.default.sys.krb5.realm = "example.com"
-        node.default['sys']['krb5']['admin_server'] = "master.example.com"
-        node.default['sys']['krb5']['master'] = "master.example.com"
-        node.default['sys']['krb5']['slave'] = "slave.example.com"
-        node.default['sys']['krb5']['use_pkinit'] = true
-        node.default['sys']['krb5']['wallet_server'] = "wallet.example.com"
-        node.automatic['domain'] = 'example.com'
-        node.automatic['fqdn'] = 'node.example.com'
-      end.converge(described_recipe)
-    end
-
-    it "configures pkinit and wallet" do
-      expect(chef_run).to create_template('/etc/krb5.conf').with(
-        :variables => {
-          :realm => "EXAMPLE.COM",
-          :admin_server => "master.example.com",
-          :servers => [ 'master.example.com', 'slave.example.com'],
-          :domain => "example.com",
-          :wallet_server => "wallet.example.com",
-          :realms => [],
-          :libdefaults => nil,
-          :use_pkinit => true
-        }
-      )
-      expect(chef_run).to render_file("/etc/krb5.conf").with_content("\tuse_pkinit = true")
-      expect(chef_run).to render_file('/etc/krb5.conf').with_content("\t\twallet_server = wallet.example.com")
-    end
-  end
-
-  context 'sys_wallet' do
+  context 'with attributes' do
     cached(:chef_run) do
       ChefSpec::SoloRunner.new do |node|
         node.default['sys']['krb5']['realm'] = 'dummy'
         node.automatic['fqdn'] = 'node.example.com'
-      end.converge(described_recipe)
-    end
-
-    it 'deploys keytab' do
-      chef_run.node.automatic['fqdn'] = 'node.example.com'
-      chef_run.converge(described_recipe)
-      expect(chef_run).to deploy_sys_wallet('host/node.example.com')
-    end
-  end
-
-  context 'sys_harry' do
-    cached(:chef_run) do
-      ChefSpec::SoloRunner.new do |node|
         node.default['sys']['krb5']['krb5.conf'] = {
           :realms => {
             'EXAMPLE.COM' => {
@@ -126,6 +36,15 @@ describe 'sys::krb5' do
       end.converge(described_recipe)
     end
 
+    it 'installs packages' do
+      expect(chef_run).to install_package('heimdal-clients')
+      expect(chef_run).to install_package('libpam-heimdal')
+      expect(chef_run).to install_package('heimdal-docs')
+      expect(chef_run).to install_package('libsasl2-modules-gssapi-heimdal')
+      expect(chef_run).to install_package('kstart')
+      expect(chef_run).to install_package('wallet-client')
+    end
+
     it 'deploys krb5.conf with two sections' do
       config = ''
       config << "[realms]\n"
@@ -143,6 +62,38 @@ describe 'sys::krb5' do
       config << "        }\n"
       expect(chef_run).to create_template('/etc/krb5.conf').with_mode('0644')
       expect(chef_run).to render_file("/etc/krb5.conf").with_content(config)
+    end
+
+    it 'deploys keytab' do
+      chef_run.node.automatic['fqdn'] = 'node.example.com'
+      chef_run.converge(described_recipe)
+      expect(chef_run).to deploy_sys_wallet('host/node.example.com')
+    end
+
+    it 'manages /etc/krb5.conf' do
+      expect(chef_run).to create_template('/etc/krb5.conf').with_mode('0644')
+      expect(chef_run).to create_template('/etc/krb5.conf').with(
+        :variables => {
+          :sections => {
+            'realms' => {
+              'EXAMPLE.COM' => {
+                'kdc' => [
+                  'kdc1.example.com',
+                  'kdc2.example.com'
+                ],
+                'admin_server' => 'kdc1.example.com',
+              }
+            },
+            'appdefaults' => {
+              'default_realm' => 'EXAMPLE.COM',
+              'wallet' => {
+                'wallet_port' => '4373',
+                'wallet_server' => 'wallet.example.com'
+              }
+            }
+          }
+        }
+      )
     end
   end
 end
