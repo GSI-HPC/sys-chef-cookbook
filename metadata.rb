41fc767c647add2d82112e54e3131fb91e7786d9
Christopher Huhn <C.Huhn@gsi.de>
2018-05-03 14:56:51 +0200

    autofs recipe reorganized

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,10 @@ maintainer_email 'hpc@gsi.de'
 license          'Apache 2.0'
 description      'System Software configuration and maintenance'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url       'https://github.com/GSI-HPC/sys-chef-cookbook'
-issues_url       'https://github.com/GSI-HPC/sys-chef-cookbook/issues'
-version          '1.39.1'
+if respond_to?(:source_url)
+  source_url       'https://github.com/GSI-HPC/sys-chef-cookbook'
+end
+if respond_to?(:issues_url)
+  issues_url       'https://github.com/GSI-HPC/sys-chef-cookbook/issues'
+end
+version          '1.40.0'
