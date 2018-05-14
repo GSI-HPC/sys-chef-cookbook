be19621564563a2a9d21f43d31a8884f2b41eba9
Christopher Huhn <C.Huhn@gsi.de>
2018-05-14 15:24:54 +0200

    Add integration test for sys::autofs

---
+++ test/integration/sys_autofs/serverspec/localhost/autofs_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe service('autofs') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+# bind-mounted /tmp
+describe file('/test/tempo/my_testfile') do
+  let(:banner) { 'Created by sys::autofs integration test' }
+
+  before do
+    # write file to /tmp
+    File.write('/tmp/my_testfile', banner)
+  end
+
+  it { should exist }
+  its(:content) { should include(banner) }
+end
