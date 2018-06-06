058cfe0dbe45fd2056393a1fe94f89e5652cee6e
Christopher Huhn <C.Huhn@gsi.de>
2018-06-06 12:58:43 +0200

    Use return to skip recipe

--- recipes/ipmi.rb
+++ recipes/ipmi.rb
@@ -2,51 +2,49 @@
 # IPMI related stuff
 #
 
+return if ! node['sys']['ipmi']['install_packages'] &&
+          node['sys']['ipmi']['overheat_protection'].empty?
 
-unless node['sys']['ipmi']['install_packages'] &&
-       node['sys']['ipmi']['overheat_protection'].empty?
+# Install the packages needed to configure/grab data from the IPMI cards.
 
-  # Install the packages needed to configure/grab data from the IPMI cards.
+# Install the command line tool to access BMC data:
+package 'ipmitool'
 
-  # Install the command line tool to access BMC data:
-  package 'ipmitool'
+# Tools used by the supplied IPMI Ohai plugin (bmc-config):
+package 'freeipmi-tools'
 
-  # Tools used by the supplied IPMI Ohai plugin (bmc-config):
-  package 'freeipmi-tools'
+# configure IPMI overheat protection:
+cookbook_file '/usr/local/sbin/ipmi-setup-overheat-protection' do
+  source 'scripts/ipmi-setup-overheat-protection'
+  mode 0o0755
+end
 
-  # configure IPMI overheat protection:
-  cookbook_file '/usr/local/sbin/ipmi-setup-overheat-protection' do
-    source 'scripts/ipmi-setup-overheat-protection'
-    mode 0o0755
-  end
-
-  if node['sys']['ipmi']['overheat_protection']['enable']
-
-    # this data structure will be filled by IPMI ohai plugin
-    #  (files/default/ohai_plugins/ipmi.rb)
-    if node['ipmi'] and node['ipmi']['pef-config']
-
-      cmd = '/usr/local/sbin/ipmi-setup-overheat-protection'
-      cmd += " -w #{node['sys']['ipmi']['overheat_protection']['warn_threshold']}" if
-        node['sys']['ipmi']['overheat_protection']['warn_threshold']
-      cmd += " -c #{node['sys']['ipmi']['overheat_protection']['crit_threshold']}" if
-        node['sys']['ipmi']['overheat_protection']['crit_threshold']
-
-      execute 'Setup IPMI Overheat Protection' do
-        command cmd
-        # only run if no such filter already exists:
-        only_if {
-          node['ipmi']['pef-config'].select{ |k,v|
-            k =~ /Event_Filter_(\d+)/ and
-              v['Enable_Filter'] == 'Yes' and
-              v['Event_Filter_Action_Power_Off'] == 'Yes' and
-              v['Sensor_Type'] == 'Temperature'
-          }.empty?
-        }
-        # don't abort, might converge only later:
-        ignore_failure true
-      end
+if node['sys']['ipmi']['overheat_protection']['enable']
+  
+  # this data structure will be filled by IPMI ohai plugin
+  #  (files/default/ohai_plugins/ipmi.rb)
+  if node['ipmi'] and node['ipmi']['pef-config']
+    
+    cmd = '/usr/local/sbin/ipmi-setup-overheat-protection'
+    cmd += " -w #{node['sys']['ipmi']['overheat_protection']['warn_threshold']}" if
+      node['sys']['ipmi']['overheat_protection']['warn_threshold']
+    cmd += " -c #{node['sys']['ipmi']['overheat_protection']['crit_threshold']}" if
+      node['sys']['ipmi']['overheat_protection']['crit_threshold']
+    
+    execute 'Setup IPMI Overheat Protection' do
+      command cmd
+      # only run if no such filter already exists:
+      only_if {
+        node['ipmi']['pef-config'].select{ |k,v|
+          k =~ /Event_Filter_(\d+)/ and
+            v['Enable_Filter'] == 'Yes' and
+            v['Event_Filter_Action_Power_Off'] == 'Yes' and
+            v['Sensor_Type'] == 'Temperature'
+        }.empty?
+      }
+      # don't abort, might converge only later:
+      ignore_failure true
     end
-
   end
+  
 end
