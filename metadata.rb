f2f8a5912ae9de56607537b40ee423327050d752
Christopher Huhn <C.Huhn@gsi.de>
2018-09-24 12:49:28 +0200

    Add chef_version and supports options

--- metadata.rb
+++ metadata.rb
@@ -10,4 +10,6 @@ end
 if respond_to?(:issues_url)
   issues_url       'https://github.com/GSI-HPC/sys-chef-cookbook/issues'
 end
+chef_version     '>= 12.0' if respond_to?(:chef_version)
+supports         'debian'
 version          '1.45.0'
