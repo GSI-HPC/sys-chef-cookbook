c9abb0fffdc2d3d3dc06224b41fb472057476ba9
Christopher Huhn <C.Huhn@gsi.de>
2019-09-09 18:48:05 +0200

    Extend integration test

--- vagrant.rb
+++ vagrant.rb
@@ -1,6 +1,8 @@
 Vagrant.configure(2) do |config|
-    config.vm.provision "shell", inline: <<-SHELL
+  # exim4-base is required as MTA for sudo test
+  config.vm.provision "shell", inline: <<-SHELL
      sudo apt-get update
      sudo DEBIAN_FRONTEND=noninteractive apt-get -y install chef
+     sudo DEBIAN_FRONTEND=noninteractive apt-get -y install exim4-base
   SHELL
 end
