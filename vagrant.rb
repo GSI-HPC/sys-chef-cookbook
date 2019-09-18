14fd06c10d0b8584fa2296d352f483133740309f
Christopher Huhn <C.Huhn@gsi.de>
2019-09-18 15:19:32 +0200

    Silence apt-get update

--- vagrant.rb
+++ vagrant.rb
@@ -1,7 +1,7 @@
 Vagrant.configure(2) do |config|
   # exim4-base is required as MTA for sudo test
   config.vm.provision "shell", inline: <<-SHELL
-     sudo apt-get update
+     sudo apt-get -qq update
      sudo DEBIAN_FRONTEND=noninteractive apt-get -y install chef
      sudo DEBIAN_FRONTEND=noninteractive apt-get -y install exim4-base
   SHELL
