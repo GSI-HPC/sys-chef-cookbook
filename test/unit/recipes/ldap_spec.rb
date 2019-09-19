9d1336c4b26c4a15890edd9b942b1646fa5fb087
Christopher Huhn <C.Huhn@gsi.de>
2019-09-19 08:10:49 +0200

    stub existance of kinit

--- test/unit/recipes/ldap_spec.rb
+++ test/unit/recipes/ldap_spec.rb
@@ -1,4 +1,34 @@
+#
+# Cookbook Name:: sys
+# Recipe:: default
+#
+# Copyright 2015 - 2019 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH
+#
+# Authors:
+#   Matthias Pausch
+#   Bastian Neuburger
+#   Dennis Klein
+#   Christopher Huhn
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 describe 'sys::ldap' do
+  before do
+    allow(File).to receive(:exist?).and_call_original
+    allow(File).to receive(:exist?).with('/usr/bin/kinit').and_return(true)
+  end
+
   let(:chef_run) do
     ChefSpec::SoloRunner.new.converge(described_recipe)
   end
