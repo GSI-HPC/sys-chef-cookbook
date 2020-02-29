3f82c9219ecdef6ae3cfc65627b185007061138d
Christopher Huhn <C.Huhn@gsi.de>
2020-02-29 17:12:00 +0100

    Config file on CentOS is /etc/ferm.conf

--- recipes/ferm.rb
+++ recipes/ferm.rb
@@ -2,7 +2,24 @@
 # Cookbook Name:: sys
 # Recipe:: ferm
 #
-# Copyright 2014, HPC Team
+# Copyright 2014-2020 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH
+#
+# Authors:
+#  Christopher Huhn   <c.huhn@gsi.de>
+#  Dennis Klein       <d.klein@gsi.de>
+#  Matthias Pausch    <m.pausch@gsi.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 class Chef::Recipe::SysFermSanityCheckError < RuntimeError
@@ -43,7 +60,7 @@ unless node['sys']['ferm']['rules'].empty?
     fermaction = :stop
   end
 
-  template '/etc/ferm/ferm.conf' do
+  template (node['platform_family'] == 'debian') ? '/etc/ferm/ferm.conf' : '/etc/ferm.conf' do
     source 'etc_ferm_ferm.conf.erb'
     mode   '0644'
     owner  'root'
