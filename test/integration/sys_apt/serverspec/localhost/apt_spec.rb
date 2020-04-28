5eb01452e3e2e1b12a2371a1083c5b08aced50d1
Christopher Huhn <C.Huhn@gsi.de>
2020-04-28 15:08:49 +0200

    Integration tests for apt almost complete

--- test/integration/sys_apt/serverspec/localhost/apt_spec.rb
+++ test/integration/sys_apt/serverspec/localhost/apt_spec.rb
@@ -28,7 +28,7 @@ describe command('dpkg --configure -a') do
   its(:stderr) { should be_empty }
 end
 
-describe command('apt-get -qq update') do
+describe command('apt-get -qq update'), if: os[:family] == 'debian' do
   its(:exit_status) { should be_zero }
   its(:stdout) { should be_empty }
   its(:stderr) { should be_empty }
@@ -46,4 +46,39 @@ describe file('/etc/apt/preferences.d/sid') do
   its(:content) { should include('Package: *') }
   its(:content) { should include('Pin: release l=Debian,n=sid') }
   its(:content) { should include('Pin-Priority: 333') }
+end
+
+describe file('/etc/apt/sources.list.d/sid.list') do
+  it { should exist }
+  its(:content) { should include('deb http://ftp.debian.org/debian sid main') }
+end
+
+describe command('apt-cache policy'), if: os[:family] == 'debian' do
+  its(:exit_status) { should be_zero }
+  its(:stderr) { should be_empty }
+  its(:stdout) do
+    should include('333 http://ftp.debian.org/debian sid/main')
+  end
+end
+
+describe command('apt-key list') do
+  its(:exit_status) { should be_zero }
+  # stderr of apt-key contains a warning when it is not a terminal
+  its(:stdout) do
+    should include('2DEC 3301 51BB 9F7D AD8B  0BDC FC32 CEEC A534 A9C6')
+  end
+  its(:stdout) do
+    should include('uid           [ unknown] Felix von Leitner <felix@fefe.de>')
+  end
+end
+
+describe package('nyancat') do
+  it { should be_installed }
+end
+
+describe command('dpkg --print-foreign-architectures') do
+  its(:exit_status) { should be_zero }
+  # TODO: requires installation of Ohai 'debian' plugin
+  # its(:stdout) { should be "i386/n" }
+  its(:stderr) { should be_empty }
 end
