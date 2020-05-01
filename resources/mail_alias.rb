391cadae2b81e0177b04fb0c17cbf436f1bc8352
Christopher Huhn <C.Huhn@gsi.de>
2020-05-01 09:56:30 +0200

    Implement a switch between old and new resource implementation based on the Chef version.
    Kudos to Dennis!
    This reverts commit cf34ad16c3813e22bd7ea20737a09aa48d6ff439.

--- resources/mail_alias.rb
+++ resources/mail_alias.rb
@@ -1,8 +1,10 @@
 #
-# Copyright 2014 - 2019, GSI Helmholtzzentrum fuer Schwerionenforschung GmbH
+# Copyright 2014-2020 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH
+#
 # Authors:
-#   Dennis Klein
-#   Christopher Huhn
+#  Christopher Huhn   <c.huhn@gsi.de>
+#  Dennis Klein       <d.klein@gsi.de>
+#  Matthias Pausch    <m.pausch@gsi.de>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,32 +19,47 @@
 # limitations under the License.
 #
 
-property :to, [Array, String],
-         # turn Strings into Arrays for simplicity:
-         coerce: proc { |t| Array(t) }
-property :aliases_file, String,  default: '/etc/aliases'
+if Gem::Requirement.new('>= 12.5')
+     .satisfied_by?(Gem::Version.new(Chef::VERSION))
 
-default_action :add
+  property :to, [Array, String],
+           # turn Strings into Arrays for simplicity:
+           coerce: proc { |t| Array(t) }
+  property :aliases_file, String,  default: '/etc/aliases'
 
-action :add do
-  new_line = "#{new_resource.name}: "
-  new_line += new_resource.to.map do |e|
-    e =~ /[:@#|]/ ? "\"#{e}\"" : e
-  end.join(', ')
+  default_action :add
 
-  replace_or_add "alias for #{new_resource.name}" do
-    path    new_resource.aliases_file
-    pattern "^#{new_resource.name}:.*"
-    line    new_line
-    backup  true        if respond_to?(:backup)
-    ignore_missing true if respond_to?(:ignore_missing)
+  action :add do
+    new_line = "#{new_resource.name}: "
+    new_line += new_resource.to.map do |e|
+      e =~ /[:@#|]/ ? "\"#{e}\"" : e
+    end.join(', ')
+
+    replace_or_add "alias for #{new_resource.name}" do
+      path    new_resource.aliases_file
+      pattern "^#{new_resource.name}:.*"
+      line    new_line
+      backup  true        if respond_to?(:backup)
+      ignore_missing true if respond_to?(:ignore_missing)
+    end
   end
-end
 
-action :remove do
-  delete_lines "alias for #{new_resource.name}" do
-    path    new_resource.aliases_file
-    pattern "^#{new_resource.name}:.*"
-    only_if { ::File.exist?(new_resource.aliases_file) }
+  action :remove do
+    delete_lines "alias for #{new_resource.name}" do
+      path    new_resource.aliases_file
+      pattern "^#{new_resource.name}:.*"
+      only_if { ::File.exist?(new_resource.aliases_file) }
+    end
   end
+else
+  actions :add, :remove
+
+  default_action :add
+
+  attribute :name, :kind_of => String, :name_attribute => true,
+            :required => true
+  attribute :to,   :kind_of => [Array, String], :default => nil
+  attribute :aliases_file, :kind_of => String,  :default => '/etc/aliases'
+
+  attr_accessor :exists
 end
