56400400dbeeb82d9aec7cc56e3f10165301c83f
Christopher Huhn <C.Huhn@gsi.de>
2020-05-31 14:46:13 +0200

    Use official SPDX identifier for the license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'sys'
 maintainer       'GSI HPC department'
 maintainer_email 'hpc@gsi.de'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'System Software configuration and maintenance'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 if respond_to?(:source_url)
