9814f81664847a2a74b6a8d4fdbacebe75038263
Christopher Huhn <C.Huhn@gsi.de>
2020-11-30 15:23:58 +0100

    Install host command for tests

--- vagrant_centos.rb
+++ vagrant_centos.rb
@@ -1,7 +1,7 @@
 Vagrant.configure(2) do |config|
   # additional tools required for integration tests:
   config.vm.provision "shell", inline: <<-SHELL
-    sudo yum -y install mailx postfix
+    sudo yum -y install bind-utils mailx postfix
     sudo systemctl start postfix
   SHELL
 end
