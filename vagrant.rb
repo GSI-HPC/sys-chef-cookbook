1b0045213e92ebff91ff5e371adbfc5c9583fb73
Christopher Huhn <C.Huhn@gsi.de>
2021-04-12 15:35:25 +0200

    Install ruby

--- vagrant.rb
+++ vagrant.rb
@@ -2,6 +2,7 @@ Vagrant.configure(2) do |config|
   # exim4-base is required as MTA for sudo test
   config.vm.provision "shell", inline: <<-SHELL
      sudo apt-get -qq update
+     sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install ruby
      sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install chef
      sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install exim4-base
      sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install shellcheck
