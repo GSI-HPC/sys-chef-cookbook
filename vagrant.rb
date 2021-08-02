bc5be71f57e83bb547023a10ffd78017ab537121
Christopher Huhn <C.Huhn@gsi.de>
2021-08-02 18:05:05 +0200

    Vagrant proxy setup

--- vagrant.rb
+++ vagrant.rb
@@ -1,6 +1,7 @@
 Vagrant.configure(2) do |config|
+  # install the Debian-provided Chef package
   # exim4-base is required as MTA for sudo test
-  config.vm.provision "shell", inline: <<-SHELL
+  config.vm.provision 'shell', inline: <<-SHELL
      sudo apt-get -qq update
      sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install chef
      sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install exim4-base
@@ -9,4 +10,11 @@ Vagrant.configure(2) do |config|
      # v2 is the last version that works with Ruby 2.1:
      sudo gem install chef-vault --version '< 4'
   SHELL
+
+  # configure proxy if required:
+  if Vagrant.has_plugin?('vagrant-proxyconf')
+    config.proxy.http     = 'http://proxy.gsi.de:3128/'
+    config.proxy.https    = 'http://proxy.gsi.de:3128/'
+    config.proxy.no_proxy = 'localhost,127.0.0.1,.gsi.de'
+  end
 end
