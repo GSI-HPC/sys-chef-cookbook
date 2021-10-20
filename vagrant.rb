ed5d3721e54d8d1c3125f9d84bf64bf4c6b1eee6
Christopher Huhn <C.Huhn@gsi.de>
2021-10-20 10:45:31 +0200

    Install mailutils, required for tests of sys::mail

--- vagrant.rb
+++ vagrant.rb
@@ -5,6 +5,7 @@ Vagrant.configure(2) do |config|
      sudo apt-get -qq update
      sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install chef
      sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install exim4-base
+     sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install mailutils
      sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install shellcheck
      sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install snmp
      # v2 is the last version that works with Ruby 2.1:
