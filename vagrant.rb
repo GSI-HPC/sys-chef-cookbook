5476bf7d6d6c2a2c626a89ce6baf99966f5ae901
Christopher Huhn <C.Huhn@gsi.de>
2021-10-20 10:45:31 +0200

    Install mailutils, required for tests of sys::mail

--- vagrant.rb
+++ vagrant.rb
@@ -5,6 +5,7 @@ Vagrant.configure(2) do |config|
      sudo apt-get -qq update
      sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install chef
      sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install exim4-base
+     sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install mailutils
      sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install shellcheck
      sudo gem install chef-vault --version '< 4'
   SHELL
