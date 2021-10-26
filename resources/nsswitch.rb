57644a045c62a83ca41b7fd8e94133b2b4e72a37
Matthias Pausch <m.pausch@gsi.de>
2021-10-26 12:42:42 +0200

    Coerce sources to a hash

--- resources/nsswitch.rb
+++ resources/nsswitch.rb
@@ -20,37 +20,39 @@
 
 provides :nsswitch
 
-property :database, String, name_property: true
-property :sources, [String, Array, Hash]
-property :notify_nsswitch_config, [true, false], default: true
-property :merge, [true, false], default: true
+  property :database, String, name_property: true
+  property :sources, [String, Array, Hash],
+    coerce: proc do |sources|
+      if sources.instance_of?(Hash)
+        sources
+      else
+        sources_as_hash = {}
+        Array(sources).each_with_index do |s, i|
+          sources_as_hash[s] = 10*(i + 1)
+        end
+        sources_as_hash
+      end
+    end
+  property :notify_nsswitch_config, [true, false], default: true
 
 action_class do
   def sources_to_hash(sources)
     return sources if sources.instance_of?(Hash)
 
-    sources_as_hash = {}
-    Array(sources).each_with_index do |s, i|
-      sources_as_hash[s] = 10*(i + 1)
-    end
-    return sources_as_hash
-  end
-end
-
-action :create do
-  return unless new_resource.notify_nsswitch_config
-  nsswitch_resource = new_resource
-  nsswitch_resource.sources = sources_to_hash(nsswitch_resource.sources)
-  with_run_context :root do
-    edit_resource('sys_nsswitch_config', 'default') do
-      action :nothing
-      old = config.dup
-      old[nsswitch_resource.database] ||= {}
-      old[nsswitch_resource.database].merge! nsswitch_resource.sources
-      config(old)
-    end
-    log "Create nsswitch.conf!" do
-      notifies :create, 'sys_nsswitch_config[default]', :delayed
+  action :create do
+    return unless new_resource.notify_nsswitch_config
+    nsswitch_resource = new_resource
+    with_run_context :root do
+      edit_resource('sys_nsswitch_config', 'default') do
+        action :nothing
+        old = config.dup
+        old[nsswitch_resource.database] ||= {}
+        old[nsswitch_resource.database].merge! nsswitch_resource.sources
+        config(old)
+      end
+      log "Create nsswitch.conf!" do
+        notifies :create, 'sys_nsswitch_config[default]', :delayed
+      end
     end
   end
 end
