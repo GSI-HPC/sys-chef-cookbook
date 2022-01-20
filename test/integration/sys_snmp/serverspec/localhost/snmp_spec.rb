a8f845bbdecfc651c7de7efa9d46274e8368b234
Christopher Huhn <C.Huhn@gsi.de>
2022-01-20 18:40:02 +0100

    Test for log level setup

--- test/integration/sys_snmp/serverspec/localhost/snmp_spec.rb
+++ test/integration/sys_snmp/serverspec/localhost/snmp_spec.rb
@@ -1,7 +1,23 @@
 #
-# checks for snmpd installation
+# Cookbook Name:: sys
+# Integration tests for snmpd setup
 #
-# Christopher Huhn 2019
+# Copyright 2019-2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH
+#
+# Authors:
+#  Christopher Huhn   <C.Huhn@gsi.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 require 'spec_helper'
@@ -37,6 +53,7 @@ end
 
 describe process('snmpd') do
   its(:user) { should eq snmp_user }
+  its(:args) { should match %r{ -LS1d } }
 end
 
 describe package(snmpwalk_package) do
