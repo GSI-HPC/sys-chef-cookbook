eb992ff4076e549e9d05e44968e987cb599591f0
Christopher Huhn <C.Huhn@gsi.de>
2022-03-25 12:33:21 +0100

    Add a comment and emit a warning

--- recipes/nftables.rb
+++ recipes/nftables.rb
@@ -2,13 +2,25 @@
 # Cookbook Name:: sys
 # Recipe:: nftables
 #
-# Copyright 2014, HPC Team
+# Copyright 2014-2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH
+#
+# Authors:
+#  Matthias Pausch   <m.pausch@gsi.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
-unless node['sys']['nftables'].empty?
+# this recipe has been superseded by sys::firewall
+return unless node['sys']['nftables']
 
-  if node['debian'] && node['debian']['codename'] && node['debian']['codename'].eql?('stretch')
-
-    
-  end
-end
+Chef::Log.warn('The sys::nftables recipe has been removed. Use sys::firewall instead')
