487c44d6f9c9474cadc9b4a39b7d056c0221075b
Matthias Pausch <m.pausch@gsi.de>
2022-05-31 14:41:03 +0200

    Do not test resource::x509_certificate on stretch or older

--- test/integration/sys_x509/serverspec/localhost/x509_spec.rb
+++ test/integration/sys_x509/serverspec/localhost/x509_spec.rb
@@ -1,20 +1,22 @@
 require 'spec_helper'
 
-describe file('/etc/ssl/certs/www-linux.gsi.de.pem') do
-  it { should exist }
-end
-
-describe file('/etc/ssl/private/www-linux.gsi.de.key') do
-  it { should exist }
-  its(:content) do
-    should match(/^-----BEGIN RSA PRIVATE KEY-----\nMIIEcgIBAAK.*/)
+if os[:release].to_i >= 10
+  describe file('/etc/ssl/certs/www-linux.gsi.de.pem') do
+    it { should exist }
   end
-end
 
-describe file('/tmp/covfefe.pem') do
-  it { should exist }
-end
+  describe file('/etc/ssl/private/www-linux.gsi.de.key') do
+    it { should exist }
+    its(:content) do
+      should match(/^-----BEGIN RSA PRIVATE KEY-----\nMIIEcgIBAAK.*/)
+    end
+  end
 
-describe x509_certificate('/tmp/covfefe.pem') do
-  its('subject') { should eq 'CN = alternativlos.org' }
+  describe file('/tmp/covfefe.pem') do
+    it { should exist }
+  end
+
+  describe x509_certificate('/tmp/covfefe.pem') do
+    its('subject') { should eq 'CN = alternativlos.org' }
+  end
 end
