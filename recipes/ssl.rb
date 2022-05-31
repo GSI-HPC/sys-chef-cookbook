c2432a28d5522b3e46f343091612869816ed9fd5
Matthias Pausch <m.pausch@gsi.de>
2022-05-31 14:23:17 +0200

    Fix sys::ssl for jessie and use x509_certificate-resource

--- recipes/ssl.rb
+++ recipes/ssl.rb
@@ -5,6 +5,7 @@
 # Copyright 2020-2021 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH
 #
 # Authors:
+#  Andre Kerkhoff     <A.Kerkhoff@gsi.de>
 #  Christopher Huhn   <C.Huhn@gsi.de>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -33,32 +34,41 @@ defaults = {
     'key_vault'     => 'ssl_keys'
   }
 }
+use_resource = Gem::Requirement.new('>= 12.15').satisfied_by?(Gem::Version.new(Chef::VERSION))
 
 node['sys']['ssl']['certs'].each do |attrs|
   cert = defaults[:cert].merge(attrs)
   cert['file'] ||= "/etc/ssl/certs/#{cert['data_bag_item']}.pem"
 
-  begin
-    file cert['file'] do
-      content data_bag_item(cert['data_bag'], cert['data_bag_item'])['file-content']
-      owner 'root'
-      group 'root'
-      mode  '0644' # this file is public
-    end
-  rescue Net::HTTPServerException
-    next
-  end
+  if use_resource
 
-  cert['key_file'] ||= "/etc/ssl/private/#{cert['data_bag_item']}.key"
-  begin
-    file cert['key_file'] do
-      content chef_vault_item(cert['key_vault'], cert['data_bag_item'])['file-content']
-      owner 'root'
-      group 'ssl-cert'
-      mode  '0640' # this file is only readable for the ssl-cert group
-      sensitive true
+    x509_certificate cert['data_bag_item']
+
+  else
+
+    begin
+      file cert['file'] do
+        content data_bag_item(cert['data_bag'], cert['data_bag_item'])['file-content']
+        owner 'root'
+        group 'root'
+        mode  '0644' # this file is public
+      end
+    rescue Net::HTTPServerException
+      next
     end
-  rescue Net::HTTPServerException, ChefVault::Exceptions => e
-    Chef::Log.warn "Could not retrieve SSL key for #{cert['data_bag_item']}: #{e}"
- end
+
+    cert['key_file'] ||= "/etc/ssl/private/#{cert['data_bag_item']}.key"
+    begin
+      file cert['key_file'] do
+        content chef_vault_item(cert['key_vault'], cert['data_bag_item'])['file-content']
+        owner 'root'
+        group 'ssl-cert'
+        mode  '0640' # this file is only readable for the ssl-cert group
+        sensitive true
+      end
+    rescue Net::HTTPServerException, ChefVault::Exceptions => e
+      Chef::Log.warn "Could not retrieve SSL key for #{cert['data_bag_item']}: #{e}"
+    end
+
+  end
 end
