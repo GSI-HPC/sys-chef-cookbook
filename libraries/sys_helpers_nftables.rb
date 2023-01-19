8c1ba04af8420cd0cd5307c993cc56a0b99eaea1
Matthias Pausch <m.pausch@gsi.de>
2023-01-19 15:39:55 +0100

    Actually rubocop suggests this

--- libraries/sys_helpers_nftables.rb
+++ libraries/sys_helpers_nftables.rb
@@ -198,6 +198,7 @@ module Sys
         when :esp, :ah
           nftables_rule << "#{ip_family} #{ip_family == :ip6 ? 'nexthdr' : 'protocol'} #{rule_resource.protocol} "
         when :none
+          nil
         else
           nftables_rule << "#{ip_family} protocol #{rule_resource.protocol} "
         end
