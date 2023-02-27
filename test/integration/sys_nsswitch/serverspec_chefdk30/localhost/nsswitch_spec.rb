9f7daa7e21e4cc1b2be1f1bfd6f719ed2c7f3fe3
Matthias Pausch <m.pausch@gsi.de>
2023-02-27 14:22:58 +0100

    Fix tests for nsswitch

--- test/integration/sys_nsswitch/serverspec_chefdk30/localhost/nsswitch_spec.rb
+++ test/integration/sys_nsswitch/serverspec_chefdk30/localhost/nsswitch_spec.rb
@@ -30,15 +30,15 @@ describe file('/etc/nsswitch.conf') do
   its(:content) { should match(/^netgroup:\s+compat$/) }
 
   # standard settings
-  its(:content) { should match(/^group:\s+compat$/) }
-  its(:content) { should match(/^shadow:\s+compat$/) }
+  its(:content) { should match(/^group:\s+files$/) }
+  its(:content) { should match(/^shadow:\s+files$/) }
   its(:content) { should match(/^gshadow:\s+files$/) }
   its(:content) { should match(/^hosts:\s+files dns$/) }
   its(:content) { should match(/^networks:\s+files$/) }
-  its(:content) { should match(/^protocols:\s+db files$/) }
-  its(:content) { should match(/^services:\s+db files$/) }
-  its(:content) { should match(/^ethers:\s+db files$/) }
-  its(:content) { should match(/^rpc:\s+db files$/) }
+  its(:content) { should match(/^protocols:\s+files$/) }
+  its(:content) { should match(/^services:\s+files$/) }
+  its(:content) { should match(/^ethers:\s+files$/) }
+  its(:content) { should match(/^rpc:\s+files$/) }
   its(:content) { should_not match(/sources:\s+nis/) }
 end
 
