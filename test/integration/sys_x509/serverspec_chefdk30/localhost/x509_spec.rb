b95bf2dab51f64b949ac90f017cbe6cef3de35c8
Matthias Pausch <m.pausch@gsi.de>
2023-08-30 13:50:03 +0200

    copy/paste-error

--- test/integration/sys_x509/serverspec_chefdk30/localhost/x509_spec.rb
+++ test/integration/sys_x509/serverspec_chefdk30/localhost/x509_spec.rb
@@ -4,7 +4,7 @@ if os[:release].to_i >= 10
   describe file('/etc/ssl/certs/www-linux.gsi.de_chain.pem') do
     it { should exist }
     its(:content) do
-      should match(/^lnwgvLxGmf4\+6iWA\+dvG3PHirgCHyWmVTOwy7\+ikhEdtd9q4\n-----END CERTIFICATE-----\n-----BEGIN CERTIFICATE-----\nMIIFrDCCBJSgAwIBAgIHG2O60B4sPTANBgkqhkiG9w0BAQsFADCBlTELMAkGA1UEGqK1chk5$/m)
+      should match(/^lnwgvLxGmf4\+6iWA\+dvG3PHirgCHyWmVTOwy7\+ikhEdtd9q4\n-----END CERTIFICATE-----\n-----BEGIN CERTIFICATE-----\nMIIFrDCCBJSgAwIBAgIHG2O60B4sPTANBgkqhkiG9w0BAQsFADCBlTELMAkGA1UE$/m)
       should match(/^LXUV2EoY6hbvVTQiGhONBg==\n-----END CERTIFICATE-----\n-----BEGIN CERTIFICATE-----\nMIIFEjCCA\/qgAwIBAgIJAOML1fivJdmBMA0GCSqGSIb3DQEBCwUAMIGCMQswCQYD$/m)
       should match(/^qK1chk5\n-----END CERTIFICATE-----\z/m)
     end
