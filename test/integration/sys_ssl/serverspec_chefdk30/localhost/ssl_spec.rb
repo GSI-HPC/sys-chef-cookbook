ce4b52ea7766e4d0da4228a310ac883f96086183
Matthias Pausch <m.pausch@gsi.de>
2023-08-30 16:02:38 +0200

    Make include-chain-option available in sys::ssl

--- test/integration/sys_ssl/serverspec_chefdk30/localhost/ssl_spec.rb
+++ test/integration/sys_ssl/serverspec_chefdk30/localhost/ssl_spec.rb
@@ -33,6 +33,23 @@ describe file('/etc/ssl/private/www-linux.gsi.de.key') do
   end
 end
 
+describe file('/etc/ssl/certs/www-linux.gsi.de_chain.pem') do
+  it { should exist }
+  its(:content) do
+    should match(/^lnwgvLxGmf4\+6iWA\+dvG3PHirgCHyWmVTOwy7\+ikhEdtd9q4\n-----END CERTIFICATE-----\n-----BEGIN CERTIFICATE-----\nMIIFrDCCBJSgAwIBAgIHG2O60B4sPTANBgkqhkiG9w0BAQsFADCBlTELMAkGA1UE$/m)
+    should match(/^LXUV2EoY6hbvVTQiGhONBg==\n-----END CERTIFICATE-----\n-----BEGIN CERTIFICATE-----\nMIIFEjCCA\/qgAwIBAgIJAOML1fivJdmBMA0GCSqGSIb3DQEBCwUAMIGCMQswCQYD$/m)
+    should match(/^GqK1chk5\n-----END CERTIFICATE-----\z/m)
+  end
+end
+
+describe file('/etc/ssl/certs/www-linux.gsi.de_no_chain.pem') do
+  it { should exist }
+  its(:content) do
+    should_not match(/^GqK1chk5$/)
+    should match(/^lnwgvLxGmf4\+6iWA\+dvG3PHirgCHyWmVTOwy7\+ikhEdtd9q4\n-----END CERTIFICATE-----\Z/m)
+  end
+end
+
 describe file('/tmp/covfefe.pem') do
   it { should exist }
 end
