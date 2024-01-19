c61219b76571e486a7e18762a0bf447dd6676101
Christopher Huhn <C.Huhn@gsi.de>
2024-01-19 18:53:27 +0100

    Adapt tests to new header template (cinc vs. chef)

--- test/integration/sys_systemd/serverspec_chefdk30/localhost/journald_spec.rb
+++ test/integration/sys_systemd/serverspec_chefdk30/localhost/journald_spec.rb
@@ -1,7 +1,7 @@
 # Cookbook Name:: sys
 # Integration tests for recipe sys::systemd systemd unit config
 #
-# Copyright 2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH
+# Copyright 2022-2024 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH
 #
 # Authors:
 #  Christopher Huhn   <c.huhn@gsi.de>
@@ -23,7 +23,7 @@ require 'spec_helper'
 
 describe file '/etc/systemd/journald.conf' do
   it { should exist }
-  its(:content) { should contain '# This file is managed by chef' }
+  its(:content) { should match %r{^# This file is managed by (chef|cinc)} }
   its(:content) { should match %r{^Storage=volatile$} }
 end
 
