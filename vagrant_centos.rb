7772d04f211aa354dd186a341bcd0b57d7d9d6b2
Christopher Huhn <C.Huhn@gsi.de>
2024-10-07 16:51:34 +0200

    Install each package in a separate command

--- vagrant_centos.rb
+++ vagrant_centos.rb
@@ -1,7 +1,9 @@
 Vagrant.configure(2) do |config|
   # additional tools required for integration tests:
   config.vm.provision "shell", inline: <<-SHELL
-    sudo yum -y install bind-utils mailx postfix
+    sudo yum -y install bind-utils
+    sudo yum -y install mailx
+    sudo yum -y install postfix
     sudo systemctl start postfix
   SHELL
 end
