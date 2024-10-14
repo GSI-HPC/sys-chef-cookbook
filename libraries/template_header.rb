cdc2d7830da894505c59ff7bdf5422d3c362f621
Christopher Huhn <C.Huhn@gsi.de>
2024-10-14 13:36:47 +0200

    Create dedicated module for template_header method (fixes #52)

--- libraries/template_header.rb
+++ libraries/template_header.rb
@@ -1,18 +1,44 @@
-class Chef::Mixin::Template::TemplateContext
-  def chef_product_name
-    begin
+#
+# Cookbook:: sys
+# Library:: template_header helper
+#
+# Copyright:: 2019-2024 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH
+#
+# Authors:
+#  Matthias Pausch    <m.pausch@gsi.de>
+#  Christopher Huhn   <c.huhn@gsi.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module Sys
+  # module for template_header
+  module TemplateHeader
+    def chef_product_name
       ChefUtils::Dist::Infra::SHORT
     rescue NameError
       'chef' # fallback if chef too old
     end
-  end
 
-  def template_header(comment = '#')
-    <<-HEADER.gsub(/^ */, "#{comment} ")
-    DO NOT CHANGE THIS FILE MANUALLY!
+    def template_header(comment = '#')
+      <<-HEADER.gsub(/^ */, "#{comment} ")
+       DO NOT CHANGE THIS FILE MANUALLY!
 
-    This file is managed by #{chef_product_name}.
-    Created by #{@cookbook_name}::#{@recipe_name} (line #{@recipe_line}) from template #{@template_name}.
-    HEADER
+       This file is managed by #{chef_product_name}.
+       Created by #{@cookbook_name}::#{@recipe_name} (line #{@recipe_line}) from template #{@template_name}
+      HEADER
+    end
   end
 end
+
+Chef::Mixin::Template::TemplateContext.include(Sys::TemplateHeader)
