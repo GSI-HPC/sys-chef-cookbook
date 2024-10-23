9d21344be2b5bf9085876e58dbc8ac2f4062c995
Christopher Huhn <C.Huhn@gsi.de>
2024-10-23 18:28:58 +0200

    Skip template information unless used in a template context

--- libraries/template_header.rb
+++ libraries/template_header.rb
@@ -31,12 +31,12 @@ module Sys
     end
 
     def template_header(comment = '#')
-      <<-HEADER.gsub(/^ */, "#{comment} ")
-       DO NOT CHANGE THIS FILE MANUALLY!
-
-       This file is managed by #{chef_product_name}.
-       Created by #{@cookbook_name}::#{@recipe_name} (line #{@recipe_line}) from template #{@template_name}
-      HEADER
+      header = "DO NOT CHANGE THIS FILE MANUALLY!\n\n" \
+               "This file is managed by #{chef_product_name}.\n"\
+               "Created by #{@cookbook_name}::#{@recipe_name} "\
+               "(line #{@recipe_line})"
+      header += " from template #{@template_name}" if @template_name
+      header.gsub(/^ */, "#{comment} ")
     end
   end
 end
