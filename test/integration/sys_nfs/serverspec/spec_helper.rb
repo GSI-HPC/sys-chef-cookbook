0b2713be596d1b4e68b88cb81dd079eed63a5b34
Christopher Huhn <C.Huhn@gsi.de>
2025-04-14 11:56:25 +0200

    Latest specinfra detects Debian Testing as 2**32-1

--- test/integration/sys_nfs/serverspec/spec_helper.rb
+++ test/integration/sys_nfs/serverspec/spec_helper.rb
@@ -2,15 +2,11 @@ require 'serverspec'
 
 set :backend, :exec
 
-# return a meaningful numeric Debian version where
-#  the value for Testing or Unstable is 1) numeric and
-#  2) larger than any sensible Debian version number
+# return a meaningful numeric Debian version
+#  casting to a float is very simplistic and misses corner cases
+#  like 8.11 should be greater than 8.7
+#  but for our use cases it will just do
 def debian_version
   return -1 unless os[:family] == 'debian'
-  if os[:release] =~ /^\d+(\.\d+)?$/
-    os[:release].to_f
-  elsif os[:release] == 'n/a' &&
-        ::File.exist?('/etc/debian_version')
-    2**32 - 1.0
-  end
+  os[:release].to_f
 end
--- test/integration/sys_nftables_empty/serverspec/spec_helper.rb
+++ test/integration/sys_nftables_empty/serverspec/spec_helper.rb
@@ -1,3 +1,12 @@
 require 'serverspec'
 
 set :backend, :exec
+
+# return a meaningful numeric Debian version
+#  casting to a float is very simplistic and misses corner cases
+#  like 8.11 should be greater than 8.7
+#  but for our use cases it will just do
+def debian_version
+  return -1 unless os[:family] == 'debian'
+  os[:release].to_f
+end
