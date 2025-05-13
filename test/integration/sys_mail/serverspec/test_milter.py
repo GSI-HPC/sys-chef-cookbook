eeb21f4c035599d10a138e756b6e551496bc460e
Christopher Huhn <C.Huhn@gsi.de>
2025-05-13 11:39:17 +0200

    Add a copyright header and some comments

--- test/integration/sys_mail/serverspec/test_milter.py
+++ test/integration/sys_mail/serverspec/test_milter.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
+#
+# Copyright:: 2025 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH
+#
+# Authors:
+#  Christopher Huhn   <C.Huhn@gsi.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""minimal Milter that adds a X-Milter-Filter header to every mail"""
 
 import Milter
 
 class TestMilter(Milter.Base):
+    """Minimal Milter"""
+
     def eom(self):
         self.addheader("X-Milter-Filter", "intelligent circuitry")
         return Milter.ACCEPT
